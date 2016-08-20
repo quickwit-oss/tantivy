2e1ad4bbce8b4af3520f2142c5c70942672f649a
Paul Masurel <paul.masurel@gmail.com>
2016-08-20 16:07:32 +0900

    Moving code the thread code to an index_worker object

--- src/indexer/mod.rs
+++ src/indexer/mod.rs
@@ -4,5 +4,7 @@ pub mod segment_serializer;
 pub mod merger;
 
 mod segment_writer;
+mod index_worker;
 
+pub use self::index_worker::IndexWorker;
 pub use self::segment_writer::SegmentWriter;
--- src/indexer/writer.rs
+++ src/indexer/writer.rs
@@ -5,23 +5,17 @@ use core::index::Index;
 use core::index::SerializableSegment;
 use core::index::Segment;
 use std::clone::Clone;
-use std::thread;
 use std::io;
-use std::thread::JoinHandle;
 use indexer::merger::IndexMerger;
-use indexer::segment_writer::SegmentWriter;
+use indexer::IndexWorker;
 use chan;
 
 use Result;
 use Error;
 
-// struct IndexWorker {
-// 	schema: Schema,
-// 	document_receiver: chan::Receiver<Document>,
-// }
 
 pub struct IndexWriter {
-	index_workers: Vec<JoinHandle<()>>,
+	index_workers: Vec<IndexWorker>,
 	index: Index,
 	schema: Schema,
 	document_sender: chan::Sender<Document>,
@@ -34,69 +28,13 @@ impl IndexWriter {
 	pub fn open(index: &Index, num_threads: usize) -> Result<IndexWriter> {
 		let schema = index.schema();
 		let (document_sender, document_receiver): (chan::Sender<Document>, chan::Receiver<Document>) = chan::sync(PIPELINE_MAX_SIZE_IN_DOCS);
-		// let workers = (0 .. num_threads).map(_ {
-			
-		// });	
-		// let index_worker = IndexWorker {
-		// 	schema: schema.clone(),
-		// 	document_receiver: document_receiver,
-		// }
-		
-				
-		let threads = (0..num_threads).map(|_|  {
-			
-			let document_receiver_clone = document_receiver.clone();
-			let mut index_clone = index.clone();
-			let schema_clone = schema.clone();
-			
-			thread::spawn(move || {
-
-				// TODO think about how to handle error within the thread
-
-				let mut docs_remaining = true;
-				while docs_remaining {
-					
-					// the first document is handled separately in order to
-					// avoid creating a new segment if there are no documents. 
-					
-					let mut doc: Document;
-					{
-						match document_receiver_clone.recv() {
-							Some(doc_) => { 
-								doc = doc_;
-							}
-							None => {
-								return;
-							}
-						}
-					}
-					
-					let segment = index_clone.new_segment();
-					let mut segment_writer = SegmentWriter::for_segment(segment.clone(), &schema_clone).unwrap();
-					segment_writer.add_document(&doc, &schema_clone).unwrap();
-					
-					for _ in 0..100_000 {
-						{
-							match document_receiver_clone.recv() {
-								Some(doc_) => {
-									doc = doc_
-								}
-								None => {
-									docs_remaining = false;
-									break;
-								}
-							}
-						}
-						segment_writer.add_document(&doc, &schema_clone).unwrap();
-					}
-					segment_writer.finalize().unwrap();
-					index_clone.publish_segment(&segment).unwrap();
-				}
-			})
-		}).collect();
+		let mut index_workers = Vec::new();
+		for _ in 0 .. num_threads {
+			index_workers.push(IndexWorker::spawn(index.clone(), document_receiver.clone()));
+		}
 		// TODO err in thread?
 		Ok(IndexWriter {
-			index_workers: threads,
+			index_workers: index_workers,
 			index: index.clone(),
 			schema: schema,
 			document_sender: document_sender,
@@ -117,7 +55,7 @@ impl IndexWriter {
 		drop(self.document_sender);
 		for worker in self.index_workers {
 			try!(worker
-				.join()
+				.wait()
 				.map_err(|e| Error::ErrorInThread(format!("{:?}", e)))
 			);
 		}
