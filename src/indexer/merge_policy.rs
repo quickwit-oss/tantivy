8534443dfbf5de0b22427bcf856a788d4b43a5a4
Paul Masurel <paul.masurel@gmail.com>
2016-10-21 08:45:50 +0900

    NOBUG Added NoMergePolicy

--- src/indexer/merge_policy.rs
+++ src/indexer/merge_policy.rs
@@ -9,3 +9,11 @@ pub struct MergeCandidate(pub Vec<SegmentId>);
 pub trait MergePolicy: marker::Send {
     fn compute_merge_candidates(&self, segments: &[SegmentMeta]) -> Vec<MergeCandidate>;
 }
+
+pub struct NoMergePolicy;
+
+impl MergePolicy for NoMergePolicy {
+    fn compute_merge_candidates(&self, segments: &[SegmentMeta]) -> Vec<MergeCandidate> {
+        Vec::new()
+    }
+}
--- src/indexer/mod.rs
+++ src/indexer/mod.rs
@@ -13,8 +13,7 @@ mod directory_lock;
 pub use self::segment_serializer::SegmentSerializer;
 pub use self::segment_writer::SegmentWriter;
 pub use self::index_writer::IndexWriter;
-pub use self::merge_policy::MergePolicy;
-pub use self::merge_policy::MergeCandidate;
 pub use self::simple_merge_policy::SimpleMergePolicy;
+pub use self::merge_policy::{NoMergePolicy, MergeCandidate, MergePolicy};
 pub use self::segment_manager::SegmentManager;
 
