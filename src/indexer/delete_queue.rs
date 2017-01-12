395cbf39139b1d59b81a5a7aab01a3b7df8a15c6
Paul Masurel <paul.masurel@gmail.com>
2017-01-12 10:09:34 +0900

    issue/43 Change the delete queue datastruct for something cleaner/functional

--- src/indexer/delete_queue.rs
+++ src/indexer/delete_queue.rs
@@ -4,83 +4,103 @@ use std::collections::HashMap;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 
+const BLOCK_SIZE: usize = 128;
 
-pub struct SuscribeHandle {
-    client_id: usize,
-    clients: Arc<RwLock<HashMap<usize, u64>>>,
+struct DeleteOperation {
+    pub opstamp: u64,
+    pub term: Term,
 }
 
-impl Drop for SuscribeHandle {
-    fn drop(&mut self) {
-        self.clients
-            .write()
-            .unwrap()
-            .remove(&self.client_id);
-    }
-}
-
-struct ClientSuscriptionRegister {
-    clients: Arc<RwLock<HashMap<usize, u64>>>,
-    client_id_autoinc: AtomicUsize,
-}
-
-impl Default for ClientSuscriptionRegister {
-    fn default() -> ClientSuscriptionRegister {
-        ClientSuscriptionRegister {
-            clients: Arc::new(RwLock::new(HashMap::new())),
-            client_id_autoinc: AtomicUsize::new(0),
-        }
-    }
-}
-
-impl ClientSuscriptionRegister {
-    fn acquire_client_id(&mut self) -> usize {
-        self.client_id_autoinc.fetch_add(1, Ordering::SeqCst)
-    }
-
-    fn suscribe(&mut self, opstamp: u64) -> SuscribeHandle {
-        let client_id = self.acquire_client_id();
-        self.clients
-            .write()
-            .unwrap()
-            .insert(client_id, opstamp);
-        SuscribeHandle {
-            client_id: client_id,
-            clients: self.clients.clone(),
-        }
-    }
-
-}
-
-
-pub struct DeleteQueue {
+struct Block {
     operations: Vec<DeleteOperation>,
-    client_subscription_register: ClientSuscriptionRegister,   
+    next: Option<SharedBlock>,
 }
 
-impl DeleteQueue {
-    pub fn push(&mut self, opstamp: u64, term: Term) {
-        self.operations.push(DeleteOperation {
-            opstamp: opstamp,
-            term: term
-        });
+impl Default for Block {
+    fn default() -> Block {
+        Block {
+            operations: Vec::with_capacity(BLOCK_SIZE),
+            next: None
+        }
     }
+}
 
-    pub fn suscribe(&mut self, opstamp: u64) -> SuscribeHandle {
-        self.client_subscription_register.suscribe(opstamp)
+#[derive(Clone)]
+struct SharedBlock {
+    inner: Arc<RwLock<Block>>,
+}
+
+impl SharedBlock {
+    // Happens a new element to the block and return 
+    // what the new head is.
+    fn enqueue(&self, delete_operation: DeleteOperation) -> Option<SharedBlock> {
+        let mut writable_block = self.inner.write().expect("Panicked while enqueueing in the delete queue.");
+        if writable_block.operations.len() >= BLOCK_SIZE {
+            let next_block = SharedBlock::default();
+            next_block.enqueue(delete_operation);
+            writable_block.next = Some(next_block.clone());
+            Some(next_block)
+        }
+        else {
+            writable_block.operations.push(delete_operation);
+            None
+        }
+    }
+    
+    fn cursor(&self,) -> DeleteQueueCursor {
+        let len = self.inner
+            .read()
+            .expect("Panicked while reading a block in the delete queue.")
+            .operations
+            .len();
+        DeleteQueueCursor {
+            block: self.clone(),
+            pos: len,
+        }
+    }
+}
+
+impl Default for SharedBlock {
+    fn default() -> SharedBlock {
+        SharedBlock {
+            inner: Arc::default()
+        }
     }
 }
 
 impl Default for DeleteQueue {
     fn default() -> DeleteQueue {
         DeleteQueue {
-            operations: Vec::new(),
-            client_subscription_register: ClientSuscriptionRegister::default(),
+            head: SharedBlock::default(),
         }
     }
 }
 
-struct DeleteOperation {
-    opstamp: u64,
-    term: Term,
+pub struct DeleteQueueCursor {
+    block: SharedBlock,
+    pos: usize,
+}
+
+
+// ----------------------------------------
+
+pub struct DeleteQueue {
+    head: SharedBlock,  
+}
+
+impl DeleteQueue {
+    
+    pub fn cursor(&self) -> DeleteQueueCursor {
+        self.head.cursor()
+    }
+    
+    pub fn push(&mut self, opstamp: u64, term: Term) {
+        let delete_operation = DeleteOperation {
+            opstamp: opstamp,
+            term: term,
+        };
+        if let Some(new_head) = self.head.enqueue(delete_operation) {
+            self.head = new_head;
+        }
+    }
 }
