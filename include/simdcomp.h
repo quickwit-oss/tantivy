f07ccd6e4fbc5bbfeb94d40e0f14bc527a7d5439
Manuel Woelker <github@manuel.woelker.org>
2017-01-26 20:28:23 +0100

    Squashed 'cpp/simdcomp/' content from commit 0dca286
    git-subtree-dir: cpp/simdcomp
    git-subtree-split: 0dca28668f1fb6d343dc3c62fa7750a00f1d7201

---
+++ include/simdcomp.h
@@ -0,0 +1,22 @@
+/**
+ * This code is released under a BSD License.
+ */
+
+#ifndef SIMDCOMP_H_
+#define SIMDCOMP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "simdbitpacking.h"
+#include "simdcomputil.h"
+#include "simdfor.h"
+#include "simdintegratedbitpacking.h"
+#include "avxbitpacking.h"
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif 
