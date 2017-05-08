3c3a2fbfe8100d5faa33e0a3deb461dc55c79528
Laurentiu Nicola <lnicola@dend.ro>
2017-05-08 07:36:15 +0300

    Remove old serialization code

--- src/schema/named_field_document.rs
+++ src/schema/named_field_document.rs
@@ -11,37 +11,3 @@ use schema::Value;
 ///
 #[derive(Serialize)]
 pub struct NamedFieldDocument(pub BTreeMap<String, Vec<Value>>);
-
-// TODO: Remove
-/*
-impl Encodable for NamedFieldDocument {
-    fn encode<S: Encoder>(&self, s: &mut S) -> Result<(), S::Error> {
-        s.emit_struct("named_field_document", self.0.len(), |s| {
-            for (i, (name, vals)) in self.0.iter().enumerate() {
-                s.emit_struct_field(name, i, |s| {
-                    for (j, val) in vals.iter().enumerate() {
-                        s.emit_seq(vals.len(), |s| {
-                            s.emit_seq_elt(j, |s| {
-                                match *val {
-                                    Value::Str(ref text) => {
-                                        s.emit_str(text)
-                                    },
-                                    Value::U64(ref val) => {
-                                        s.emit_u64(*val)
-                                    }
-                                    Value::I64(ref val) => {
-                                        s.emit_i64(*val)
-                                    }
-                                }
-                            })
-                        })?;
-                    }
-                    Ok(())
-                    
-                })?;
-            }
-            Ok(())
-        })
-    }
-}
-*/
