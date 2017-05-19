5c36f91c6d649cd235d74f80caea377423889911
Maciej Dziardziel <m.dziardziel@cv-library.co.uk>
2017-05-19 12:46:38 +0100

    unify i64 and u64 handling

--- src/collector/facet_collector.rs
+++ src/collector/facet_collector.rs
@@ -1,7 +1,9 @@
+use std::cmp::Eq;
 use std::collections::HashMap;
+use std::hash::Hash;
 
 use collector::Collector;
-use fastfield::{FastFieldReader, U64FastFieldReader};
+use fastfield::{FastFieldReader, I64FastFieldReader, U64FastFieldReader};
 use schema::Field;
 
 use DocId;
@@ -11,16 +13,25 @@ use SegmentReader;
 use SegmentLocalId;
 
 
-/// faceting for u64 fast field
-pub struct FastFieldValueFacet {
-    counters: HashMap<u64, u64>,
+/// faceting for i64/u64 fast field
+pub struct FastFieldValueFacet<T>
+    where
+        T: FastFieldReader,
+        T::ValueType: Eq + Hash
+{
+    counters: HashMap<T::ValueType, u64>,
     field: Field,
-    ff_reader: Option<U64FastFieldReader>,
+    ff_reader: Option<T>,
 }
 
 
-impl FastFieldValueFacet {
-    fn new(field: Field) -> FastFieldValueFacet {
+impl <T>FastFieldValueFacet<T>
+    where
+        T: FastFieldReader,
+        T::ValueType: Eq + Hash
+{
+    fn new(field: Field) -> FastFieldValueFacet<T>
+    {
         FastFieldValueFacet {
             counters: HashMap::new(),
             field: field,
@@ -30,7 +41,11 @@ impl FastFieldValueFacet {
 }
 
 
-impl Collector for FastFieldValueFacet {
+impl <T>Collector for FastFieldValueFacet<T>
+    where
+        T: FastFieldReader,
+        T::ValueType: Eq + Hash
+{
 
     fn set_segment(&mut self, _: SegmentLocalId, reader: &SegmentReader) -> Result<()> {
         self.ff_reader = Some(try!(reader.get_fast_field_reader(self.field)));
@@ -45,7 +60,8 @@ impl Collector for FastFieldValueFacet {
 
 
 enum FacedType {
-    FastField(FastFieldValueFacet)
+    FastFieldI64(FastFieldValueFacet<I64FastFieldReader>),
+    FastFieldU64(FastFieldValueFacet<U64FastFieldReader>),
 }
 
 
@@ -70,7 +86,8 @@ impl Collector for FacetCollector {
     fn set_segment(&mut self, segment_id: SegmentLocalId, reader: &SegmentReader) -> Result<()> {
        for facet_type in self.facets.iter_mut() {
             match facet_type {
-                 &mut FacedType::FastField(ref mut fast_field_value_facet) => fast_field_value_facet.set_segment(segment_id, reader)
+                 &mut FacedType::FastFieldI64(ref mut fast_field_value_facet) => fast_field_value_facet.set_segment(segment_id, reader),
+                 &mut FacedType::FastFieldU64(ref mut fast_field_value_facet) => fast_field_value_facet.set_segment(segment_id, reader)
             };
         };
         Ok(())
@@ -79,7 +96,8 @@ impl Collector for FacetCollector {
     fn collect(&mut self, doc: DocId, score: Score) {
         for facet_type in self.facets.iter_mut() {
             match facet_type {
-                 &mut FacedType::FastField(ref mut fast_field_value_facet) => fast_field_value_facet.collect(doc, score)
+                 &mut FacedType::FastFieldI64(ref mut fast_field_value_facet) => fast_field_value_facet.collect(doc, score),
+                 &mut FacedType::FastFieldU64(ref mut fast_field_value_facet) => fast_field_value_facet.collect(doc, score)
             }
         };
     }
@@ -100,16 +118,22 @@ mod tests {
     // make sure we have facet counters correctly filled
     fn test_facet_collector_results() {
         let mut schema_builder = schema::SchemaBuilder::new();
-        let num_field = schema_builder.add_u64_field(
-            "num",
+        let num_field_i64 = schema_builder.add_i64_field(
+            "num_i64",
             schema::IntOptions::default()
                 .set_fast()
                 .set_indexed()
             );
+        let num_field_u64 = schema_builder.add_u64_field(
+            "num_u64",
+            schema::IntOptions::default()
+                .set_fast()
+                .set_indexed()
+            );
+
         let text_field = schema_builder.add_text_field(
             "text",
             schema::TextOptions::default()
-                //.set_stored()
                 .set_indexing_options(schema::TextIndexingOptions::Untokenized)
             );
 
@@ -121,7 +145,8 @@ mod tests {
             {
                 for i in 1..11 {
                     let mut doc = Document::default();
-                    doc.add_u64(num_field, i % 2);
+                    doc.add_i64(num_field_i64, i % 2);
+                    doc.add_u64(num_field_u64, (i % 2) as u64);
                     doc.add_text(text_field, "text");
                     index_writer.add_document(doc);
                 }
@@ -131,18 +156,24 @@ mod tests {
 
         index.load_searchers().unwrap();
         let searcher = index.searcher();
-        let ffvf = FastFieldValueFacet::new(num_field);
-        let mut facet_collector = FacetCollector::new(vec![FacedType::FastField(ffvf)]);
+        let ffvf_i64 = FastFieldValueFacet::new(num_field_i64);
+        let ffvf_u64 = FastFieldValueFacet::new(num_field_u64);
+        let mut facet_collector = FacetCollector::new(vec![FacedType::FastFieldI64(ffvf_i64), FacedType::FastFieldU64(ffvf_u64)]);
 
         let query_parser = QueryParser::new(schema, vec!(text_field));
         let query = query_parser.parse_query("text:text").unwrap();
         query.search(&searcher, &mut facet_collector).unwrap();
         for facet in facet_collector.facets {
             match facet {
-                FacedType::FastField(ffvf) => {
+                FacedType::FastFieldI64(ffvf) => {
+                    assert_eq!(ffvf.counters[&0], 5);
+                    assert_eq!(ffvf.counters[&1], 5);
+                },
+                FacedType::FastFieldU64(ffvf) => {
                     assert_eq!(ffvf.counters[&0], 5);
                     assert_eq!(ffvf.counters[&1], 5);
                 }
+
             }
         }
     }
