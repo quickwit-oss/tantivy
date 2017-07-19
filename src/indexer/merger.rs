77d8e81ae4d7b0141e53be85937b6975297aed66
Paul Masurel <paul.masurel@gmail.com>
2017-07-19 11:08:42 +0900

    issue/17 Slightly more explicit error message

--- src/indexer/merger.rs
+++ src/indexer/merger.rs
@@ -240,7 +240,7 @@ impl IndexMerger {
                     .field_type()
                     .get_segment_postings_option()
                     .expect("Encounterred a field that is not supposed to be
-                         indexed. Have you modified the index?");
+                         indexed. Have you modified the schema?");
                 last_field = Some(current_field);
 
                 // it is perfectly safe to call `.new_field`
