51cd8087c928207e5a0f443214732cea3c2bbc62
Dru Sellers <dru@drusellers.com>
2017-08-07 08:48:34 -0500

    simplifing s3 implementation to follow RAMDirectory

--- src/directory/s3_directory.rs
+++ src/directory/s3_directory.rs
@@ -10,6 +10,7 @@ use memmap::{Mmap, Protection};
 use std::collections::hash_map::Entry as HashMapEntry;
 use std::collections::HashMap;
 use std::convert::From;
+use std::default::Default;
 use std::fmt;
 use std::fs::{self, File};
 use std::fs::OpenOptions;
@@ -24,125 +25,14 @@ use std::sync::RwLock;
 use std::sync::Weak;
 use tempdir::TempDir;
 use rusoto_core::{DefaultCredentialsProvider, Region, default_tls_client};
-use rusoto_s3::{S3, S3Client, HeadBucketRequest};
+use rusoto_s3::{S3, S3Client, HeadBucketRequest, GetObjectRequest};
 
-fn open_mmap(full_path: &PathBuf) -> result::Result<Option<Arc<Mmap>>, OpenReadError> {
-    let file = File::open(&full_path).map_err(|e| if e.kind() ==
-        io::ErrorKind::NotFound
-    {
-        OpenReadError::FileDoesNotExist(full_path.clone())
-    } else {
-        OpenReadError::IOError(IOError::with_path(full_path.to_owned(), e))
-    })?;
+#[derive(Clone)]
+struct InnerDirectory(Arc<RwLock<HashMap<PathBuf, Arc<Vec<u8>>>>>);
 
-    let meta_data = file.metadata().map_err(|e| {
-        IOError::with_path(full_path.to_owned(), e)
-    })?;
-    if meta_data.len() == 0 {
-        // if the file size is 0, it will not be possible
-        // to mmap the file, so we return an anonymous mmap_cache
-        // instead.
-        return Ok(None);
-    }
-    match Mmap::open(&file, Protection::Read) {
-        Ok(mmap) => Ok(Some(Arc::new(mmap))),
-        Err(e) => Err(IOError::with_path(full_path.to_owned(), e))?,
-    }
-
-}
-
-#[derive(Default, Clone, Debug, Serialize, Deserialize)]
-pub struct CacheCounters {
-    // Number of time the cache prevents to call `mmap`
-    pub hit: usize,
-    // Number of time tantivy had to call `mmap`
-    // as no entry was in the cache.
-    pub miss_empty: usize,
-    // Number of time tantivy had to call `mmap`
-    // as the entry in the cache was evinced.
-    pub miss_weak: usize,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct CacheInfo {
-    pub counters: CacheCounters,
-    pub mmapped: Vec<PathBuf>,
-}
-
-struct MmapCache {
-    counters: CacheCounters,
-    cache: HashMap<PathBuf, Weak<Mmap>>,
-    purge_weak_limit: usize,
-}
-
-const STARTING_PURGE_WEAK_LIMIT: usize = 1_000;
-
-impl Default for MmapCache {
-    fn default() -> MmapCache {
-        MmapCache {
-            counters: CacheCounters::default(),
-            cache: HashMap::new(),
-            purge_weak_limit: STARTING_PURGE_WEAK_LIMIT,
-        }
-    }
-}
-
-
-impl MmapCache {
-    fn cleanup(&mut self) {
-        let previous_cache_size = self.cache.len();
-        let mut new_cache = HashMap::new();
-        mem::swap(&mut new_cache, &mut self.cache);
-        self.cache = new_cache
-            .into_iter()
-            .filter(|&(_, ref weak_ref)| weak_ref.upgrade().is_some())
-            .collect();
-        if self.cache.len() == previous_cache_size {
-            self.purge_weak_limit *= 2;
-        }
-    }
-
-    fn get_info(&mut self) -> CacheInfo {
-        self.cleanup();
-        let paths: Vec<PathBuf> = self.cache.keys().cloned().collect();
-        CacheInfo {
-            counters: self.counters.clone(),
-            mmapped: paths,
-        }
-    }
-
-    fn get_mmap(&mut self, full_path: PathBuf) -> Result<Option<Arc<Mmap>>, OpenReadError> {
-        // if we exceed this limit, then we go through the weak
-        // and remove those that are obsolete.
-        if self.cache.len() > self.purge_weak_limit {
-            self.cleanup();
-        }
-        Ok(match self.cache.entry(full_path.clone()) {
-            HashMapEntry::Occupied(mut occupied_entry) => {
-                if let Some(mmap_arc) = occupied_entry.get().upgrade() {
-                    self.counters.hit += 1;
-                    Some(mmap_arc.clone())
-                } else {
-                    // The entry exists but the weak ref has been destroyed.
-                    self.counters.miss_weak += 1;
-                    if let Some(mmap_arc) = open_mmap(&full_path)? {
-                        occupied_entry.insert(Arc::downgrade(&mmap_arc));
-                        Some(mmap_arc)
-                    } else {
-                        None
-                    }
-                }
-            }
-            HashMapEntry::Vacant(vacant_entry) => {
-                self.counters.miss_empty += 1;
-                if let Some(mmap_arc) = open_mmap(&full_path)? {
-                    vacant_entry.insert(Arc::downgrade(&mmap_arc));
-                    Some(mmap_arc)
-                } else {
-                    None
-                }
-            }
-        })
+impl InnerDirectory {
+    fn new() -> InnerDirectory {
+        InnerDirectory(Arc::new(RwLock::new(HashMap::new())))
     }
 }
 
@@ -155,8 +45,7 @@ pub struct S3Directory {
     root_path: PathBuf,
     bucket: String,
     region: Region,
-    mmap_cache: Arc<RwLock<MmapCache>>,
-    _temp_directory: Arc<Option<TempDir>>,
+    fs: InnerDirectory,
 }
 
 impl fmt::Debug for S3Directory {
@@ -190,6 +79,7 @@ impl S3Directory {
             OpenDirectoryError::DoesNotExist(PathBuf::from("/bad/creds"))
         })?;
 
+        // TODO: do I want to save this off?
         let s3 = S3Client::new(client, provider, region.clone());
 
         // does bucket exist?
@@ -203,10 +93,25 @@ impl S3Directory {
             bucket,
             region: region,
             root_path: PathBuf::from(directory_path),
-            mmap_cache: Arc::new(RwLock::new(MmapCache::default())),
-            _temp_directory: Arc::new(None),
+            fs: InnerDirectory::new(),
         })
 
+    }
+
+    fn get_client(&self) -> Box<S3> {
+        let client = default_tls_client()
+            .map_err(|_| {
+                OpenReadError::FileDoesNotExist(PathBuf::from("/bad/tls/client"))
+            })
+            .unwrap();
+
+        let provider = DefaultCredentialsProvider::new()
+            .map_err(|_| {
+                OpenReadError::FileDoesNotExist(PathBuf::from("/bad/creds"))
+            })
+            .unwrap();
+
+        Box::new(S3Client::new(client, provider, self.region.clone()))
     }
 
     /// Joins a relative_path to the directory `root_path`
@@ -214,155 +119,75 @@ impl S3Directory {
     fn resolve_path(&self, relative_path: &Path) -> PathBuf {
         self.root_path.join(relative_path)
     }
-
-    /// Returns some statistical information
-    /// about the Mmap cache.
-    ///
-    /// The `MmapDirectory` embeds a `MmapDirectory`
-    /// to avoid multiplying the `mmap` system calls.
-    pub fn get_cache_info(&mut self) -> CacheInfo {
-        self.mmap_cache
-            .write()
-            .expect("Mmap cache lock is poisoned.")
-            .get_info()
-    }
 }
-
-/// This Write wraps a File, but has the specificity of
-/// call `sync_all` on flush.
-struct SafeFileWriter(File);
-
-impl SafeFileWriter {
-    fn new(file: File) -> SafeFileWriter {
-        SafeFileWriter(file)
-    }
-}
-
-impl Write for SafeFileWriter {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.0.write(buf)
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        try!(self.0.flush());
-        self.0.sync_all()
-    }
-}
-
-impl Seek for SafeFileWriter {
-    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
-        self.0.seek(pos)
-    }
-}
-
 
 impl Directory for S3Directory {
     fn open_read(&self, path: &Path) -> result::Result<ReadOnlySource, OpenReadError> {
         debug!("Open Read {:?}", path);
-        let full_path = self.resolve_path(path);
 
-        let mut mmap_cache = self.mmap_cache.write().map_err(|_| {
+        let cache = self.fs.0.read().map_err(|_| {
             let msg = format!(
-                "Failed to acquired write lock \
-                                            on mmap cache while reading {:?}",
+                "Failed to acquire read lock for the \
+                                            directory when trying to read {:?}",
                 path
             );
-            IOError::with_path(path.to_owned(), make_io_err(msg))
+            let io_err = make_io_err(msg);
+            OpenReadError::IOError(IOError::with_path(path.to_owned(), io_err))
         })?;
 
-        Ok(
-            mmap_cache
-                .get_mmap(full_path)?
-                .map(MmapReadOnly::from)
-                .map(ReadOnlySource::Mmap)
-                .unwrap_or_else(|| ReadOnlySource::Anonymous(SharedVecSlice::empty())),
-        )
+        if !cache.contains_key(path) {
+            let full_path = self.resolve_path(path);
+
+            let mut map = self.fs.0.write().map_err(|_| {
+                let msg = format!(
+                    "Failed to acquire write lock for the \
+                                            directory when trying to read {:?}",
+                    path
+                );
+                let io_err = make_io_err(msg);
+                OpenReadError::IOError(IOError::with_path(path.to_owned(), io_err))
+            })?;
+
+            let s3 = self.get_client();
+
+            let obj = s3.get_object(&GetObjectRequest {
+                bucket: self.bucket.clone(),
+                // TODO: this may not be the best approach
+                key: full_path.clone().into_os_string().into_string().unwrap(),
+                ..Default::default()
+            }).map_err(|_| {
+                    let msg = format!("No key found for {:?}", path);
+                    let io_err = make_io_err(msg);
+                    OpenReadError::IOError(IOError::with_path(path.to_owned(), io_err))
+                })?;
+
+
+            map.insert(PathBuf::from(path), Arc::new(obj.body.unwrap()));
+        }
+
+        let data = cache.get(path).unwrap();
+
+        Ok(ReadOnlySource::Anonymous(SharedVecSlice::new(data.clone())))
     }
 
     fn open_write(&mut self, path: &Path) -> Result<WritePtr, OpenWriteError> {
-        debug!("Open Write {:?}", path);
-        let full_path = self.resolve_path(path);
-
-        let open_res = OpenOptions::new().write(true).create_new(true).open(
-            full_path,
-        );
-
-        let mut file = open_res.map_err(|err| if err.kind() ==
-            io::ErrorKind::AlreadyExists
-        {
-            OpenWriteError::FileAlreadyExists(path.to_owned())
-        } else {
-            IOError::with_path(path.to_owned(), err).into()
-        })?;
-
-        // making sure the file is created.
-        file.flush().map_err(
-            |e| IOError::with_path(path.to_owned(), e),
-        )?;
-
-        let writer = SafeFileWriter::new(file);
-        Ok(BufWriter::new(Box::new(writer)))
+        unimplemented!()
     }
 
     fn delete(&self, path: &Path) -> result::Result<(), DeleteError> {
-        debug!("Deleting file {:?}", path);
-        let full_path = self.resolve_path(path);
-        let mut mmap_cache = self.mmap_cache.write().map_err(|_| {
-            let msg = format!(
-                "Failed to acquired write lock \
-                                            on mmap cache while deleting {:?}",
-                path
-            );
-            IOError::with_path(path.to_owned(), make_io_err(msg))
-        })?;
-        // Removing the entry in the MMap cache.
-        // The munmap will appear on Drop,
-        // when the last reference is gone.
-        mmap_cache.cache.remove(&full_path);
-        match fs::remove_file(&full_path) {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                if e.kind() == io::ErrorKind::NotFound {
-                    Err(DeleteError::FileDoesNotExist(path.to_owned()))
-                } else {
-                    Err(IOError::with_path(path.to_owned(), e).into())
-                }
-            }
-        }
+        unimplemented!()
     }
 
     fn exists(&self, path: &Path) -> bool {
-        let full_path = self.resolve_path(path);
-        full_path.exists()
+        unimplemented!()
     }
 
     fn atomic_read(&self, path: &Path) -> Result<Vec<u8>, OpenReadError> {
-        let full_path = self.resolve_path(path);
-        let mut buffer = Vec::new();
-        match File::open(&full_path) {
-            Ok(mut file) => {
-                file.read_to_end(&mut buffer).map_err(|e| {
-                    IOError::with_path(path.to_owned(), e)
-                })?;
-                Ok(buffer)
-            }
-            Err(e) => {
-                if e.kind() == io::ErrorKind::NotFound {
-                    Err(OpenReadError::FileDoesNotExist(path.to_owned()))
-                } else {
-                    Err(IOError::with_path(path.to_owned(), e).into())
-                }
-            }
-        }
-
+        unimplemented!()
     }
 
     fn atomic_write(&mut self, path: &Path, data: &[u8]) -> io::Result<()> {
-        debug!("Atomic Write {:?}", path);
-        let full_path = self.resolve_path(path);
-        let meta_file = atomicwrites::AtomicFile::new(full_path, atomicwrites::AllowOverwrite);
-        try!(meta_file.write(|f| f.write_all(data)));
-        Ok(())
+        unimplemented!()
     }
 
     fn box_clone(&self) -> Box<Directory> {
@@ -392,7 +217,6 @@ mod tests {
             "tantivy-test-bucket".to_string(),
             &PathBuf::from("/"),
         ).unwrap();
-
     }
 
     #[test]
@@ -403,7 +227,6 @@ mod tests {
             "tantivy-test-bucket-nope".to_string(),
             &PathBuf::from("/"),
         ).unwrap();
-
     }
 
     #[test]
@@ -417,7 +240,6 @@ mod tests {
             "tantivy-test-bucket".to_string(),
             &PathBuf::from("/"),
         ).unwrap();
-
     }
 
     #[test]
