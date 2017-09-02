71366b9a56cbceca44e840a2f2ce10f1dc54adcc
Paul Masurel <paul.masurel@gmail.com>
2017-09-02 15:55:23 +0900

    issue/197 Remove logic that prevents leak from crossbeam MsQueue. (#212)
    Closes #197

--- src/core/pool.rs
+++ src/core/pool.rs
@@ -11,43 +11,19 @@ pub struct GenerationItem<T> {
 }
 
 
-// See https://github.com/crossbeam-rs/crossbeam/issues/91
-struct NonLeakingMsQueue<T> {
-    underlying_queue: MsQueue<T>,
-}
 
-impl<T> Default for NonLeakingMsQueue<T> {
-    fn default() -> NonLeakingMsQueue<T> {
-        NonLeakingMsQueue { underlying_queue: MsQueue::new() }
-    }
-}
-
-impl<T> NonLeakingMsQueue<T> {
-    fn pop(&self) -> T {
-        self.underlying_queue.pop()
-    }
-
-    fn push(&self, el: T) {
-        self.underlying_queue.push(el);
-    }
-}
-
-impl<T> Drop for NonLeakingMsQueue<T> {
-    fn drop(&mut self) {
-        while let Some(_popped_item_to_be_dropped) = self.underlying_queue.try_pop() {}
-    }
-}
 
 pub struct Pool<T> {
-    queue: Arc<NonLeakingMsQueue<GenerationItem<T>>>,
+    queue: Arc<MsQueue<GenerationItem<T>>>,
     freshest_generation: AtomicUsize,
     next_generation: AtomicUsize,
 }
 
 impl<T> Pool<T> {
     pub fn new() -> Pool<T> {
+        let queue = Arc::new(MsQueue::new());
         Pool {
-            queue: Arc::default(),
+            queue: queue,
             freshest_generation: AtomicUsize::default(),
             next_generation: AtomicUsize::default(),
         }
@@ -108,7 +84,7 @@ impl<T> Pool<T> {
 
 pub struct LeasedItem<T> {
     gen_item: Option<GenerationItem<T>>,
-    recycle_queue: Arc<NonLeakingMsQueue<GenerationItem<T>>>,
+    recycle_queue: Arc<MsQueue<GenerationItem<T>>>,
 }
 
 impl<T> Deref for LeasedItem<T> {
