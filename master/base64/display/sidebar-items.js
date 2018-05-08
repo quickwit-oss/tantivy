e0c6dfeb624720224b589bff4706f10bb9ab79b8
Travis CI User <travis@example.org>
2018-05-08 02:50:41 +0000

    Automatic Travis documentation build

---
+++ master/base64/display/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["DisplayError","Errors that can occur initializing a Base64Display."]],"struct":[["Base64Display","A convenience wrapper for base64'ing bytes into a format string without heap allocation."]]});
