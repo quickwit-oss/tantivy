56492affd8c2ee08de627359f48f749181aa1c5d
Dru Sellers <dru@drusellers.com>
2018-06-16 10:23:57 -0500

    Regex Query Doc Test

--- src/query/regex_query.rs
+++ src/query/regex_query.rs
@@ -9,6 +9,60 @@ use Searcher;
 // A Regex Query matches all of the documents
 /// containing a specific term that matches
 /// a regex pattern
+/// A Fuzzy Query matches all of the documents
+/// containing a specific term that is within
+/// Levenshtein distance
+///
+/// ```rust
+/// #[macro_use]
+/// extern crate tantivy;
+/// use tantivy::schema::{SchemaBuilder, TEXT};
+/// use tantivy::{Index, Result, Term};
+/// use tantivy::collector::{CountCollector, TopCollector, chain};
+/// use tantivy::query::RegexQuery;
+///
+/// # fn main() { example().unwrap(); }
+/// fn example() -> Result<()> {
+///     let mut schema_builder = SchemaBuilder::new();
+///     let title = schema_builder.add_text_field("title", TEXT);
+///     let schema = schema_builder.build();
+///     let index = Index::create_in_ram(schema);
+///     {
+///         let mut index_writer = index.writer(3_000_000)?;
+///         index_writer.add_document(doc!(
+///             title => "The Name of the Wind",
+///         ));
+///         index_writer.add_document(doc!(
+///             title => "The Diary of Muadib",
+///         ));
+///         index_writer.add_document(doc!(
+///             title => "A Dairy Cow",
+///         ));
+///         index_writer.add_document(doc!(
+///             title => "The Diary of a Young Girl",
+///         ));
+///         index_writer.commit().unwrap();
+///     }
+///
+///     index.load_searchers()?;
+///     let searcher = index.searcher();
+///
+///     {
+///         let mut top_collector = TopCollector::with_limit(2);
+///         let mut count_collector = CountCollector::default();
+///         {
+///             let mut collectors = chain().push(&mut top_collector).push(&mut count_collector);
+///             let term = Term::from_field_text(title, "Diary");
+///             let query = RegexQuery::new("d[ai]{2}ry".to_string(), title);
+///             searcher.search(&query, &mut collectors).unwrap();
+///         }
+///         assert_eq!(count_collector.count(), 3);
+///         assert!(top_collector.at_capacity());
+///     }
+///
+///     Ok(())
+/// }
+/// ```
 #[derive(Debug, Clone)]
 pub struct RegexQuery {
   regex_pattern: String,
