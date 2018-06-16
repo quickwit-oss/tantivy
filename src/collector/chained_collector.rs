c14039efe648cfb3e5a1f5b41ffda674bb9af41e
Dru Sellers <dru@drusellers.com>
2018-06-16 08:29:42 -0500

    Add ChainedCollector Doc Test

--- src/collector/chained_collector.rs
+++ src/collector/chained_collector.rs
@@ -25,6 +25,57 @@ impl Collector for DoNothingCollector {
 /// Zero-cost abstraction used to collect on multiple collectors.
 /// This contraption is only usable if the type of your collectors
 /// are known at compile time.
+///
+/// ```rust
+/// #[macro_use]
+/// extern crate tantivy;
+/// use tantivy::schema::{SchemaBuilder, TEXT};
+/// use tantivy::{Index, Result};
+/// use tantivy::collector::{CountCollector, TopCollector, chain};
+/// use tantivy::query::QueryParser;
+///
+/// # fn main() { example().unwrap(); }
+/// fn example() -> Result<()> {
+///     let mut schema_builder = SchemaBuilder::new();
+///     let title = schema_builder.add_text_field("title", TEXT);
+///     let schema = schema_builder.build();
+///     let index = Index::create_in_ram(schema);
+///     {
+///         let mut index_writer = index.writer(3_000_000)?;
+///         index_writer.add_document(doc!(
+///             title => "The Name of the Wind",
+///         ));
+///         index_writer.add_document(doc!(
+///             title => "The Diary of Muadib",
+///         ));
+///         index_writer.add_document(doc!(
+///             title => "A Dairy Cow",
+///         ));
+///         index_writer.add_document(doc!(
+///             title => "The Diary of a Young Girl",
+///         ));
+///         index_writer.commit().unwrap();
+///     }
+///
+///     index.load_searchers()?;
+///     let searcher = index.searcher();
+///
+///     {
+///         let mut top_collector = TopCollector::with_limit(2);
+///         let mut count_collector = CountCollector::default();
+///         {
+///             let mut collectors = chain().push(&mut top_collector).push(&mut count_collector);
+///             let query_parser = QueryParser::for_index(&index, vec![title]);
+///             let query = query_parser.parse_query("diary")?;
+///             searcher.search(&*query, &mut collectors).unwrap();
+///         }
+///         assert_eq!(count_collector.count(), 2);
+///         assert!(top_collector.at_capacity());
+///     }
+///
+///     Ok(())
+/// }
+/// ```
 pub struct ChainedCollector<Left: Collector, Right: Collector> {
     left: Left,
     right: Right,
