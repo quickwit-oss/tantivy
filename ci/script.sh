cbfb2fe19d36d567bc502ae246ea70aa0b6d3370
Paul Masurel <paul.masurel@gmail.com>
2018-08-13 10:38:01 +0900

    Avoid building twice when doing code coverage

--- ci/script.sh
+++ ci/script.sh
@@ -1,25 +1,23 @@
+#!/usr/bin/env bash
+
 # This script takes care of testing your crate
 
 set -ex
 
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
-
-    if [ ! -z $DISABLE_TESTS ]; then
-        return
-    fi
-
     if [ ! -z $CODECOV ]; then
         echo "Codecov"
         cargo build --verbose && cargo coverage --verbose && bash <(curl -s https://codecov.io/bash) -s target/kcov
+    else
+        echo "Build"
+        cross build --target $TARGET
+        cross build --target $TARGET --release
+        if [ ! -z $DISABLE_TESTS ]; then
+            return
+        fi
+        echo "Test"
+        cross test --target $TARGET
     fi
-
-    cross test --target $TARGET
-    # cross test --target $TARGET --release
-
-    # cross run --target $TARGET
-    # cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
