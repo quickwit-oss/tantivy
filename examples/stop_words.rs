98c7fbdc6f37469a981c3a3555727debc6780ca7
Paul Masurel <paul.masurel@gmail.com>
2018-09-06 10:11:54 +0900

    Issue/378 (#392)
    * Added failing unit test
    
    * Closes #378. Handling queries that end up empty after going through the analyzer.
    
    * Fixed stop word example

--- examples/stop_words.rs
+++ examples/stop_words.rs
@@ -23,7 +23,6 @@ use tantivy::Index;
 
 fn main() -> tantivy::Result<()> {
   // this example assumes you understand the content in `basic_search`
-  let index_path = TempDir::new("tantivy_stopwords_example_dir")?;
   let mut schema_builder = SchemaBuilder::default();
 
   // This configures your custom options for how tantivy will
@@ -31,36 +30,36 @@ fn main() -> tantivy::Result<()> {
   // to note is that we are setting the tokenizer to `stoppy`
   // which will be defined and registered below.
   let text_field_indexing = TextFieldIndexing::default()
-    .set_tokenizer("stoppy")
-    .set_index_option(IndexRecordOption::WithFreqsAndPositions);
+      .set_tokenizer("stoppy")
+      .set_index_option(IndexRecordOption::WithFreqsAndPositions);
   let text_options = TextOptions::default()
-    .set_indexing_options(text_field_indexing)
-    .set_stored();
+      .set_indexing_options(text_field_indexing)
+      .set_stored();
 
   // Our first field is title.
   schema_builder.add_text_field("title", text_options);
 
   // Our second field is body.
   let text_field_indexing = TextFieldIndexing::default()
-    .set_tokenizer("stoppy")
-    .set_index_option(IndexRecordOption::WithFreqsAndPositions);
+      .set_tokenizer("stoppy")
+      .set_index_option(IndexRecordOption::WithFreqsAndPositions);
   let text_options = TextOptions::default()
-    .set_indexing_options(text_field_indexing)
-    .set_stored();
+      .set_indexing_options(text_field_indexing)
+      .set_stored();
   schema_builder.add_text_field("body", text_options);
 
   let schema = schema_builder.build();
 
-  let index = Index::create_in_dir(&index_path, schema.clone())?;
+  let index = Index::create_in_ram(schema.clone());
 
   // This tokenizer lowers all of the text (to help with stop word matching)
   // then removes all instances of `the` and `and` from the corpus
   let tokenizer = SimpleTokenizer
-    .filter(LowerCaser)
-    .filter(StopWordFilter::remove(vec![
-      "the".to_string(),
-      "and".to_string(),
-    ]));
+      .filter(LowerCaser)
+      .filter(StopWordFilter::remove(vec![
+        "the".to_string(),
+        "and".to_string(),
+      ]));
 
   index.tokenizers().register("stoppy", tokenizer);
 
@@ -76,16 +75,16 @@ fn main() -> tantivy::Result<()> {
   ));
 
   index_writer.add_document(doc!(
-        title => "Of Mice and Men",
-        body => "A few miles south of Soledad, the Salinas River drops in close to the hillside \
-                bank and runs deep and green. The water is warm too, for it has slipped twinkling \
-                over the yellow sands in the sunlight before reaching the narrow pool. On one \
-                side of the river the golden foothill slopes curve up to the strong and rocky \
-                Gabilan Mountains, but on the valley side the water is lined with trees—willows \
-                fresh and green with every spring, carrying in their lower leaf junctures the \
-                debris of the winter’s flooding; and sycamores with mottled, white, recumbent \
-                limbs and branches that arch over the pool"
-    ));
+      title => "Of Mice and Men",
+      body => "A few miles south of Soledad, the Salinas River drops in close to the hillside \
+              bank and runs deep and green. The water is warm too, for it has slipped twinkling \
+              over the yellow sands in the sunlight before reaching the narrow pool. On one \
+              side of the river the golden foothill slopes curve up to the strong and rocky \
+              Gabilan Mountains, but on the valley side the water is lined with trees—willows \
+              fresh and green with every spring, carrying in their lower leaf junctures the \
+              debris of the winter’s flooding; and sycamores with mottled, white, recumbent \
+              limbs and branches that arch over the pool"
+  ));
 
   index_writer.add_document(doc!(
        title => "Frankenstein",
@@ -103,14 +102,9 @@ fn main() -> tantivy::Result<()> {
 
   let query_parser = QueryParser::for_index(&index, vec![title, body]);
 
-  // this will have NO hits because it was filtered out
-  // because the query is run through the analyzer you
-  // actually will get an error here because the query becomes
-  // empty
-  assert!(query_parser.parse_query("the").is_err());
-
-  // this will have hits
-  let query = query_parser.parse_query("is")?;
+  // stop words are applied on the query as well.
+  // The following will be equivalent to `title:frankenstein`
+  let query = query_parser.parse_query("title:\"the Frankenstein\"")?;
 
   let mut top_collector = TopCollector::with_limit(10);
 
@@ -125,5 +119,3 @@ fn main() -> tantivy::Result<()> {
 
   Ok(())
 }
-
-use tempdir::TempDir;
