67af6de9d60ad3c69a91829c71a2d31bac1ece37
Paul Masurel <paul.masurel@gmail.com>
2018-12-17 17:04:04 +0900

    Remove murmurhash

--- src/postings/stacker/mod.rs
+++ src/postings/stacker/mod.rs
@@ -1,9 +1,7 @@
 mod expull;
 mod memory_arena;
-mod murmurhash2;
 mod term_hashmap;
 
 pub use self::expull::ExpUnrolledLinkedList;
 pub use self::memory_arena::{Addr, ArenaStorable, MemoryArena};
-use self::murmurhash2::murmurhash2;
 pub use self::term_hashmap::{compute_table_size, TermHashMap};
