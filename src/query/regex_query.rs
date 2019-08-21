6998dd2290807cb5099a54427ad504b2cf88f2c8
Alberto Piai <apiai@sensational.ch>
2019-08-21 13:54:57 +0200

    TMP RegexQuery: reference the regex in specialized_weight()

--- src/query/regex_query.rs
+++ src/query/regex_query.rs
@@ -3,9 +3,8 @@ use crate::query::{AutomatonWeight, Query, Weight};
 use crate::schema::Field;
 use crate::Result;
 use crate::Searcher;
-use std::clone::Clone;
 use std::sync::Arc;
-use tantivy_fst::{Automaton, Regex};
+use tantivy_fst::{Regex};
 
 /// A Regex Query matches all of the documents
 /// containing a specific term that matches
@@ -52,30 +51,8 @@ use tantivy_fst::{Automaton, Regex};
 /// ```
 #[derive(Debug, Clone)]
 pub struct RegexQuery {
-    regex: ShareableRegex,
+    regex: Arc<Regex>,
     field: Field,
-}
-
-#[derive(Debug, Clone)]
-struct ShareableRegex(Arc<Regex>);
-
-impl Automaton for ShareableRegex {
-    type State = <Regex as Automaton>::State;
-
-    #[inline]
-    fn start(&self) -> Self::State {
-        self.0.start()
-    }
-
-    #[inline]
-    fn is_match(&self, state: &Self::State) -> bool {
-        self.0.is_match(state)
-    }
-
-    #[inline]
-    fn accept(&self, state: &Self::State, byte: u8) -> Self::State {
-        self.0.accept(state, byte)
-    }
 }
 
 impl RegexQuery {
@@ -84,22 +61,19 @@ impl RegexQuery {
         let regex = Regex::new(&regex_pattern)
             .map_err(|_| TantivyError::InvalidArgument(regex_pattern.to_owned()))?;
 
-        Ok(RegexQuery {
-            regex: ShareableRegex(Arc::new(regex)),
-            field,
-        })
+        Ok(RegexQuery::from_regex(regex, field))
     }
 
     /// Creates a new RegexQuery from a fully built Regex
     pub fn from_regex<T: Into<Arc<Regex>>>(regex: T, field: Field) -> Self {
         RegexQuery {
-            regex: ShareableRegex(regex.into()),
+            regex: regex.into(),
             field,
         }
     }
 
-    fn specialized_weight(&self) -> AutomatonWeight<ShareableRegex> {
-        AutomatonWeight::new(self.field, self.regex.clone())
+    fn specialized_weight(&self) -> AutomatonWeight<&Regex> {
+        AutomatonWeight::new(self.field, &self.regex)
     }
 }
 
