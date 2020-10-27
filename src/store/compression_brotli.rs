2eb5326aa4d47c977a83672fcc4b216eb7167c07
Paul Masurel <paul.masurel@gmail.com>
2020-10-27 14:00:14 +0900

    Fixing compilation

--- src/store/compression_brotli.rs
+++ src/store/compression_brotli.rs
@@ -1,21 +1,20 @@
-use std::io::{self, Cursor};
+use std::io;
 
 /// Name of the compression scheme used in the doc store.
 ///
 /// This name is appended to the version string of tantivy.
 pub const COMPRESSION: &'static str = "brotli";
 
-pub fn compress(uncompressed: &[u8], compressed: &mut Vec<u8>) -> io::Result<()> {
+pub fn compress(mut uncompressed: &[u8], compressed: &mut Vec<u8>) -> io::Result<()> {
     let mut params = brotli::enc::BrotliEncoderParams::default();
     params.quality = 5;
-
     compressed.clear();
-    brotli::BrotliCompress(uncompressed, compressed, &params)?;
+    brotli::BrotliCompress(&mut uncompressed, compressed, &params)?;
     Ok(())
 }
 
-pub fn decompress(compressed: &[u8], decompressed: &mut Vec<u8>) -> io::Result<()> {
+pub fn decompress(mut compressed: &[u8], decompressed: &mut Vec<u8>) -> io::Result<()> {
     decompressed.clear();
-    brotli::BrotliDecompress(compressed, decompressed)?;
+    brotli::BrotliDecompress(&mut compressed, decompressed)?;
     Ok(())
 }
