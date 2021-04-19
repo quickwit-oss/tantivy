c2fdc605697843aa3cb7b5515a20d1d1bc3a72e4
Pascal Seitz <pascal.seitz@gmail.com>
2021-04-19 10:55:44 +0200

    fix snap version, fix naming

--- src/store/compression_lz4_block.rs
+++ src/store/compression_lz4_block.rs
@@ -12,11 +12,8 @@ pub fn compress(uncompressed: &[u8], compressed: &mut Vec<u8>) -> io::Result<()>
 
     compressed.extend_from_slice(&[0, 0, 0, 0]);
     compress_into(uncompressed, compressed);
-    let size = uncompressed.len() as u32;
-    compressed[0] = size as u8;
-    compressed[1] = (size >> 8) as u8;
-    compressed[2] = (size >> 16) as u8;
-    compressed[3] = (size >> 24) as u8;
+    let num_bytes = uncompressed.len() as u32;
+    compressed[0..4].copy_from_slice(&num_bytes.to_le_bytes());
     Ok(())
 }
 
@@ -24,9 +21,12 @@ pub fn decompress(compressed: &[u8], decompressed: &mut Vec<u8>) -> io::Result<(
     decompressed.clear();
     //next lz4_flex version will support slice as input parameter.
     //this will make the usage much less ugly
-    let size = compressed.get(..4).ok_or(io::ErrorKind::InvalidData)?;
-    let size: &[u8; 4] = size.try_into().unwrap();
-    let uncompressed_size = u32::from_le_bytes(*size) as usize;
+    let uncompressed_size_bytes: &[u8; 4] = compressed
+        .get(..4)
+        .ok_or(io::ErrorKind::InvalidData)?
+        .try_into()
+        .unwrap();
+    let uncompressed_size = u32::from_le_bytes(*uncompressed_size_bytes) as usize;
     // reserve more than required, because blocked writes may write out of bounds, will be improved
     // with lz4_flex 1.0
     decompressed.reserve(uncompressed_size + 4 + 24);
