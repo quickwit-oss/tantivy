089410914894da1922840d54846188c801e0f754
shikhar <shikhar@schmizz.net>
2021-11-15 20:12:28 -0500

    Add Compressor::NoOp

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -183,6 +183,10 @@ pub mod tests {
         Ok(())
     }
 
+    #[test]
+    fn test_store_noop() -> crate::Result<()> {
+        test_store(Compressor::NoOp)
+    }
     #[cfg(feature = "lz4-compression")]
     #[test]
     fn test_store_lz4_block() -> crate::Result<()> {
