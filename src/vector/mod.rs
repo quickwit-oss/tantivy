92e4233ebd7f9d5228418623403aa7e750140eb6
Alberto López Sánchez <alberto.lopez.s@outlook.es>
2021-11-23 18:08:19 +0100

    add vector reader

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -2,4 +2,4 @@ mod vector_writer;
 mod vector_reader;
 
 pub use self::vector_writer::VectorWriter;
-pub use self::vector_reader::VectorReader;
+pub use self::vector_reader::{VectorReader, VectorReaders};
--- src/vector/vector_reader.rs
+++ src/vector/vector_reader.rs
@@ -1,16 +1,43 @@
-use crate::space_usage::PerFieldSpaceUsage;
+use std::path::PathBuf;
+
+use crate::{DocId, schema::Field, space_usage::PerFieldSpaceUsage};
+
+
+pub struct VectorReaders {
+    segment_path: PathBuf
+}
+
+impl VectorReaders {
+    pub fn new(path: PathBuf) -> VectorReaders {
+        trace!("New VectorReaders created! {:?}.", path);
+        VectorReaders{ segment_path: path }
+    }
+
+    pub fn open_read(&self, field: Field) -> VectorReader {
+        let path = field.field_id().to_string();
+        VectorReader::new(self.segment_path.join(path))
+
+    }
+
+    pub fn space_usage(&self) -> PerFieldSpaceUsage {
+        todo!();
+    }
+}
+
+
 
 pub struct VectorReader {
     
 }
 
 impl VectorReader {
-    pub fn new() -> VectorReader {
-        trace!("New vector reader created! I need the path where I can find my vectors.");
+    pub fn new(path: PathBuf) -> VectorReader {
+        trace!("New vector reader created! {:?}.", path);
         VectorReader{}
     }
 
-    pub fn space_usage(&self) -> PerFieldSpaceUsage {
+    pub fn search(&self, vector: Vec<f32>, limit: usize) -> Vec<DocId>{
         todo!();
     }
+
 }
