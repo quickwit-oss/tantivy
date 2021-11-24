8a9343a001090e91ed857efe532bd3f3bf44e176
Alberto López Sánchez <alberto.lopez.s@outlook.es>
2021-11-24 16:17:39 +0100

    add VectorScorer

--- src/query/vector_query.rs
+++ src/query/vector_query.rs
@@ -1,9 +1,11 @@
 use common::BitSet;
+use qdrant_segment::types::ScoreType;
 
-use crate::{Score, Searcher, SegmentReader, schema::Field};
+use crate::{DocId, DocSet, Score, Searcher, SegmentReader, TERMINATED, schema::Field};
 
-use super::{BitSetDocSet, ConstScorer, Query, Weight};
+use super::{BitSetDocSet, ConstScorer, Query, Scorer, Weight};
 
+/// VectorQuery, query to look for similar documents to this vector.
 #[derive(Clone, Debug)]
 pub struct VectorQuery {
     field: Field,
@@ -11,10 +13,11 @@ pub struct VectorQuery {
 }
 
 impl VectorQuery {
+    /// Creates a new VectorQuery
     pub fn new(field: Field, vector: Vec<f32>) -> VectorQuery {
         VectorQuery {
             field,
-            vector
+            vector: vector.clone()
         }
     }
 }
@@ -31,7 +34,7 @@ impl Query for VectorQuery {
         
         Ok(Box::new(VectorWeight {
             field: self.field,
-            vector: self.vector
+            vector: self.vector.clone()
         }))
     }
 }
@@ -47,27 +50,60 @@ impl Weight for VectorWeight {
         reader: &SegmentReader,
         boost: Score,
     ) -> crate::Result<Box<dyn super::Scorer>> {
-        let max_doc = reader.max_doc();
-        let mut doc_bitset = BitSet::with_max_value(max_doc);
 
         let reader = reader.vector_reader(self.field)?;
-        let docs = reader.search(self.vector, 50);
+        // TODO: Pass limit.
+        let docs = reader.search(&self.vector, 50);
 
-        for doc in docs {
-            doc_bitset.insert(doc);
-        }
-
-        let doc_bitset = BitSetDocSet::from(doc_bitset);
-        Ok(Box::new(ConstScorer::new(doc_bitset, boost)))
+        Ok(Box::new(VectorScorer::from_docs(docs)))
     }
 
     fn explain(
         &self,
-        reader: &crate::SegmentReader,
-        doc: crate::DocId,
+        _reader: &crate::SegmentReader,
+        _doc: crate::DocId,
     ) -> crate::Result<super::Explanation> {
 
 
         todo!()
+    }
+}
+
+pub struct VectorScorer {
+    docs: Vec<(DocId, ScoreType)>,
+    i: usize
+}
+
+impl VectorScorer {
+    pub fn from_docs(docs: Vec<(DocId, ScoreType)>) -> VectorScorer {
+        VectorScorer {
+            docs,
+            i: 0
+        }
+    }
+}
+
+impl Scorer for VectorScorer {
+    fn score(&mut self) -> Score {
+        self.docs[self.i].1
+    }
+}
+
+impl DocSet for VectorScorer {
+    fn advance(&mut self) -> crate::DocId {
+        if self.i < self.docs.len() {
+            self.i += 1;
+        } else {
+            self.i = TERMINATED as usize;
+        }
+        self.i as u32
+    }
+
+    fn doc(&self) -> crate::DocId {
+        self.docs[self.i].0
+    }
+
+    fn size_hint(&self) -> u32 {
+        self.docs.len() as u32
     }
 }
