950175f4c92338554a3272d4985077c66d1d526a
saroh <325288+saroh@users.noreply.github.com>
2021-12-28 22:24:12 +0100

    doc(termdict) expose structs also add merger doc + lint refs #1232

--- src/termdict/mod.rs
+++ src/termdict/mod.rs
@@ -6,7 +6,7 @@ about the term.
 Internally, the term dictionary relies on the `fst` crate to store
 a sorted mapping that associate each term to its rank in the lexicographical order.
 For instance, in a dictionary containing the sorted terms "abba", "bjork", "blur" and "donovan",
-the `TermOrdinal` are respectively `0`, `1`, `2`, and `3`.
+the [TermOrdinal] are respectively `0`, `1`, `2`, and `3`.
 
 For `u64`-terms, tantivy explicitely uses a `BigEndian` representation to ensure that the
 lexicographical order matches the natural order of integers.
@@ -20,37 +20,18 @@ as `u64`.
 A second datastructure makes it possible to access a [`TermInfo`](../postings/struct.TermInfo.html).
 */
 
-use tantivy_fst::automaton::AlwaysMatch;
-
 mod fst_termdict;
 use fst_termdict as termdict;
 
 mod merger;
 
-#[cfg(test)]
-mod tests;
+pub use self::merger::TermMerger;
+pub use self::termdict::TermDictionary;
+pub use self::termdict::TermDictionaryBuilder;
+pub use self::termdict::TermStreamer;
 
 /// Position of the term in the sorted list of terms.
 pub type TermOrdinal = u64;
 
-/// The term dictionary contains all of the terms in
-/// `tantivy index` in a sorted manner.
-pub type TermDictionary = self::termdict::TermDictionary;
-
-/// Builder for the new term dictionary.
-///
-/// Inserting must be done in the order of the `keys`.
-pub type TermDictionaryBuilder<W> = self::termdict::TermDictionaryBuilder<W>;
-
-/// Given a list of sorted term streams,
-/// returns an iterator over sorted unique terms.
-///
-/// The item yield is actually a pair with
-/// - the term
-/// - a slice with the ordinal of the segments containing
-/// the terms.
-pub type TermMerger<'a> = self::merger::TermMerger<'a>;
-
-/// `TermStreamer` acts as a cursor over a range of terms of a segment.
-/// Terms are guaranteed to be sorted.
-pub type TermStreamer<'a, A = AlwaysMatch> = self::termdict::TermStreamer<'a, A>;
+#[cfg(test)]
+mod tests;
