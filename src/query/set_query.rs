313c9c38a940f33579db0d3cb71f0ca9783f9cc1
trinity-1686a <trinity@quickwit.io>
2022-09-27 12:00:51 +0200

    address review comments
    support using different fields
    use less and_then and more if-let

--- src/query/set_query.rs
+++ src/query/set_query.rs
@@ -1,56 +1,73 @@
+use std::collections::HashMap;
+
 use tantivy_fst::raw::CompiledAddr;
 use tantivy_fst::{Automaton, Map};
 
-use crate::query::{AutomatonWeight, Query, Weight};
+use crate::query::score_combiner::DoNothingCombiner;
+use crate::query::{AutomatonWeight, BooleanWeight, Occur, Query, Weight};
 use crate::schema::Field;
 use crate::{Searcher, Term};
 
 /// A Term Set Query matches all of the documents containing any of the Term provided
-///
-/// Terms not using the right Field are discared.
 #[derive(Debug, Clone)]
 pub struct TermSetQuery {
-    field: Field,
-    terms: Vec<Term>,
+    terms_map: HashMap<Field, Vec<Term>>,
 }
 
 impl TermSetQuery {
     /// Create a Term Set Query
-    pub fn new<T: IntoIterator<Item = Term>>(field: Field, terms: T) -> Self {
-        let mut terms: Vec<_> = terms
+    pub fn new<T: IntoIterator<Item = Term>>(terms: T) -> Self {
+        let mut terms_map: HashMap<_, Vec<_>> = terms
             .into_iter()
-            .filter(|key| key.field() == field)
-            .collect();
-        terms.sort_unstable();
-        terms.dedup();
-        TermSetQuery { field, terms }
+            .map(|term| (term.field(), term))
+            .fold(HashMap::new(), |mut map, (field, term)| {
+                map.entry(field).or_default().push(term);
+                map
+            });
+        for terms in terms_map.values_mut() {
+            terms.sort_unstable();
+            terms.dedup();
+        }
+        TermSetQuery { terms_map }
     }
 
     fn specialized_weight(
         &self,
         searcher: &Searcher,
-    ) -> crate::Result<AutomatonWeight<SetDfaWrapper>> {
-        let field_entry = searcher.schema().get_field_entry(self.field);
-        let field_type = field_entry.field_type();
-        if !field_type.is_indexed() {
-            let error_msg = format!("Field {:?} is not indexed.", field_entry.name());
-            return Err(crate::TantivyError::SchemaError(error_msg));
+    ) -> crate::Result<BooleanWeight<DoNothingCombiner>> {
+        let mut sub_queries: Vec<(_, Box<dyn Weight>)> = Vec::with_capacity(self.terms_map.len());
+
+        for (&field, sorted_terms) in self.terms_map.iter() {
+            let field_entry = searcher.schema().get_field_entry(field);
+            let field_type = field_entry.field_type();
+            if !field_type.is_indexed() {
+                let error_msg = format!("Field {:?} is not indexed.", field_entry.name());
+                return Err(crate::TantivyError::SchemaError(error_msg));
+            }
+            let field_type = field_type.value_type();
+
+            // In practice this won't fail because:
+            // - we are writing to memory, so no IoError
+            // - Terms are ordered
+            let map = Map::from_iter(
+                sorted_terms
+                    .iter()
+                    .filter(|key| key.typ() == field_type)
+                    .map(|key| (key.value_bytes(), 0)),
+            )
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+
+            sub_queries.push((
+                Occur::Should,
+                Box::new(AutomatonWeight::new(field, SetDfaWrapper(map))),
+            ));
         }
-        let field_type = field_type.value_type();
 
-        // In practice this won't fail because:
-        // - we are writing to memory, so no IoError
-        // - BTreeSet are ordered, and we limit ourselves to values with a fixed prefix (which we
-        // strip), so Map::from_iter get values in order
-        let map = Map::from_iter(
-            self.terms
-                .iter()
-                .filter(|key| key.typ() == field_type)
-                .map(|key| (key.value_bytes(), 0)),
-        )
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-
-        Ok(AutomatonWeight::new(self.field, SetDfaWrapper(map)))
+        Ok(BooleanWeight::new(
+            sub_queries,
+            false,
+            Box::new(|| DoNothingCombiner),
+        ))
     }
 }
 
@@ -69,20 +86,23 @@ struct SetDfaWrapper(Map<Vec<u8>>);
 impl Automaton for SetDfaWrapper {
     type State = Option<CompiledAddr>;
 
-    fn start(&self) -> Self::State {
+    fn start(&self) -> Option<CompiledAddr> {
         Some(self.0.as_ref().root().addr())
     }
-    fn is_match(&self, state: &Self::State) -> bool {
-        state
-            .map(|s| self.0.as_ref().node(s).is_final())
-            .unwrap_or(false)
+
+    fn is_match(&self, state_opt: &Option<CompiledAddr>) -> bool {
+        if let Some(state) = state_opt {
+            self.0.as_ref().node(*state).is_final()
+        } else {
+            false
+        }
     }
-    fn accept(&self, state: &Self::State, byte: u8) -> Self::State {
-        state.and_then(|state| {
-            let state = self.0.as_ref().node(state);
-            let transition = state.find_input(byte)?;
-            Some(state.transition_addr(transition))
-        })
+
+    fn accept(&self, state_opt: &Option<CompiledAddr>, byte: u8) -> Option<CompiledAddr> {
+        let state = state_opt.as_ref()?;
+        let node = self.0.as_ref().node(*state);
+        let transition = node.find_input(byte)?;
+        Some(node.transition_addr(transition))
     }
 
     fn can_match(&self, state: &Self::State) -> bool {
@@ -119,6 +139,10 @@ mod tests {
                 field1 => "doc3",
                 field2 => "val3",
             ))?;
+            index_writer.add_document(doc!(
+                field1 => "val3",
+                field2 => "doc3",
+            ))?;
             index_writer.commit()?;
         }
         let reader = index.reader()?;
@@ -126,9 +150,9 @@ mod tests {
 
         {
             // single element
-            let mut terms = vec![Term::from_field_text(field1, "doc1")];
+            let terms = vec![Term::from_field_text(field1, "doc1")];
 
-            let term_set_query = TermSetQuery::new(field1, terms);
+            let term_set_query = TermSetQuery::new(terms);
             let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(2))?;
             assert_eq!(top_docs.len(), 1, "Expected 1 document");
             let (score, _) = top_docs[0];
@@ -137,21 +161,21 @@ mod tests {
 
         {
             // single element, absent
-            let mut terms = vec![Term::from_field_text(field1, "doc4")];
+            let terms = vec![Term::from_field_text(field1, "doc4")];
 
-            let term_set_query = TermSetQuery::new(field1, terms);
+            let term_set_query = TermSetQuery::new(terms);
             let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(1))?;
             assert!(top_docs.is_empty(), "Expected 0 document");
         }
 
         {
             // multiple elements
-            let mut terms = vec![
+            let terms = vec![
                 Term::from_field_text(field1, "doc1"),
                 Term::from_field_text(field1, "doc2"),
             ];
 
-            let term_set_query = TermSetQuery::new(field1, terms);
+            let term_set_query = TermSetQuery::new(terms);
             let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(2))?;
             assert_eq!(top_docs.len(), 2, "Expected 2 documents");
             for (score, _) in top_docs {
@@ -160,40 +184,44 @@ mod tests {
         }
 
         {
-            // single element, wrong field
-            let mut terms = vec![Term::from_field_text(field1, "doc1")];
-
-            let term_set_query = TermSetQuery::new(field2, terms);
-            let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(1))?;
-            assert!(top_docs.is_empty(), "Expected 0 document");
-        }
-        {
             // multiple elements, mixed fields
-            let mut terms = vec![
+            let terms = vec![
                 Term::from_field_text(field1, "doc1"),
                 Term::from_field_text(field1, "doc1"),
                 Term::from_field_text(field2, "val2"),
             ];
 
-            let term_set_query = TermSetQuery::new(field1, terms.clone());
-            let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(2))?;
+            let term_set_query = TermSetQuery::new(terms);
+            let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(3))?;
 
+            assert_eq!(top_docs.len(), 2, "Expected 2 document");
+            for (score, _) in top_docs {
+                assert_nearly_equals!(1.0, score);
+            }
+        }
+
+        {
+            // no field crosstalk
+            let terms = vec![Term::from_field_text(field1, "doc3")];
+
+            let term_set_query = TermSetQuery::new(terms);
+            let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(3))?;
             assert_eq!(top_docs.len(), 1, "Expected 1 document");
-            let (score, _) = top_docs[0];
-            assert_nearly_equals!(1.0, score);
 
-            let term_set_query = TermSetQuery::new(field2, terms.clone());
-            let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(2))?;
+            let terms = vec![Term::from_field_text(field2, "doc3")];
 
+            let term_set_query = TermSetQuery::new(terms);
+            let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(3))?;
             assert_eq!(top_docs.len(), 1, "Expected 1 document");
-            let (score, _) = top_docs[0];
-            assert_nearly_equals!(1.0, score);
-            let term_set_query = TermSetQuery::new(field1, terms.clone());
-            let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(2))?;
 
-            assert_eq!(top_docs.len(), 1, "Expected 1 document");
-            let (score, _) = top_docs[0];
-            assert_nearly_equals!(1.0, score);
+            let terms = vec![
+                Term::from_field_text(field1, "doc3"),
+                Term::from_field_text(field2, "doc3"),
+            ];
+
+            let term_set_query = TermSetQuery::new(terms);
+            let top_docs = searcher.search(&term_set_query, &TopDocs::with_limit(3))?;
+            assert_eq!(top_docs.len(), 2, "Expected 2 document");
         }
 
         Ok(())
