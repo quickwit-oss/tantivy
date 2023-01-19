ed7ce3d9092f694af80eea689ac05c77b23ade16
Paul Masurel <paul@quickwit.io>
2023-01-19 11:51:29 +0900

    Columnar merge

--- columnar/src/columnar/mod.rs
+++ columnar/src/columnar/mod.rs
@@ -1,27 +1,8 @@
-// Copyright (C) 2022 Quickwit, Inc.
-//
-// Quickwit is offered under the AGPL v3.0 and as commercial software.
-// For commercial licensing, contact us at hello@quickwit.io.
-//
-// AGPL:
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-//
-
 mod column_type;
 mod format_version;
 mod reader;
 mod writer;
+mod merge;
 
 pub use column_type::ColumnType;
 pub use reader::ColumnarReader;
