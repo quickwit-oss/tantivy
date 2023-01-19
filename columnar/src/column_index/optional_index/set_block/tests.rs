8b780badcc7f2e9b590322e1094eef52aca1da9a
PSeitz <PSeitz@users.noreply.github.com>
2023-01-19 06:38:30 +0100

    add columnar to workspace

--- columnar/src/column_index/optional_index/set_block/tests.rs
+++ columnar/src/column_index/optional_index/set_block/tests.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
-use crate::column_index::optional_index::set_block::dense::DENSE_BLOCK_NUM_BYTES;
-use crate::column_index::optional_index::set_block::{DenseBlockCodec, SparseBlockCodec};
+use crate::column_index::optional_index::set_block::{
+    DenseBlockCodec, SparseBlockCodec, DENSE_BLOCK_NUM_BYTES,
+};
 use crate::column_index::optional_index::{Set, SetCodec};
 
 fn test_set_helper<C: SetCodec<Item = u16>>(vals: &[u16]) -> usize {
