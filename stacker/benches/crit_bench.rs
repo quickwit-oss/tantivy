c89a2fd3e091ca1754a8c9885e7aa580b8ee3664
Pascal Seitz <pascal.seitz@gmail.com>
2023-03-13 21:05:44 +0800

    refactor arena hashmap
    add inlines
    remove occupied array and use table_entry.is_empty instead (saves 4 bytes per entry)
    reduce saturation threshold from 1/3 to 1/2 to reduce memory
    use u32 for UnorderedId (we have the 4billion limit anyways on the Columnar stuff)
    fix naming LinearProbing
    remove byteorder dependency
    
    memory consumption went down from 2Gb to 1.8GB on indexing wikipedia dataset in tantivy

--- stacker/benches/crit_bench.rs
+++ stacker/benches/crit_bench.rs
@@ -1,15 +1,11 @@
 #![allow(dead_code)]
 extern crate criterion;
 
+use criterion::*;
+use rand::SeedableRng;
 use tantivy_stacker::ArenaHashMap;
 
-use criterion::*;
-
 const ALICE: &str = include_str!("../../benches/alice.txt");
-const HDFS: &str = include_str!("../../benches/hdfs.json");
-
-//const ALL: &[(&str, &str)] = &[("alice", ALICE), ("hdfs", HDFS)];
-const ALL: &[(&str, &str)] = &[("alice", ALICE)];
 
 fn bench_hashmap_throughput(c: &mut Criterion) {
     let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Linear);
@@ -17,23 +13,47 @@ fn bench_hashmap_throughput(c: &mut Criterion) {
     let mut group = c.benchmark_group("CreateHashMap");
     group.plot_config(plot_config);
 
-    for (input_name, input) in ALL {
-        let input_bytes = input.len() as u64;
-        group.throughput(Throughput::Bytes(input_bytes));
+    let input_name = "alice";
+    let input_bytes = ALICE.len() as u64;
+    group.throughput(Throughput::Bytes(input_bytes));
 
-        group.bench_with_input(
-            BenchmarkId::new(input_name.to_string(), input_bytes),
-            &input,
-            |b, i| b.iter(|| create_hash_map(i)),
-        );
-    }
+    group.bench_with_input(
+        BenchmarkId::new(input_name.to_string(), input_bytes),
+        &ALICE,
+        |b, i| b.iter(|| create_hash_map(i.split_whitespace().map(|el| el.as_bytes()))),
+    );
+    // numbers
+    let input_bytes = 1_000_000 * 8 as u64;
+    group.throughput(Throughput::Bytes(input_bytes));
+
+    group.bench_with_input(
+        BenchmarkId::new("numbers".to_string(), input_bytes),
+        &(0..1_000_000u64),
+        |b, i| b.iter(|| create_hash_map(i.clone().map(|el| el.to_le_bytes()))),
+    );
+
+    // numbers zipf
+    use rand::distributions::Distribution;
+    use rand::rngs::StdRng;
+    let mut rng = StdRng::from_seed([3u8; 32]);
+    let zipf = zipf::ZipfDistribution::new(10_000, 1.03).unwrap();
+
+    let input_bytes = 1_000_000 * 8 as u64;
+    group.throughput(Throughput::Bytes(input_bytes));
+
+    group.bench_with_input(
+        BenchmarkId::new("numbers_zipf".to_string(), input_bytes),
+        &(0..1_000_000u64),
+        |b, i| b.iter(|| create_hash_map(i.clone().map(|_el| zipf.sample(&mut rng).to_le_bytes()))),
+    );
+
     group.finish();
 }
 
-fn create_hash_map(terms: &str) -> ArenaHashMap {
+fn create_hash_map<'a, T: AsRef<[u8]>>(terms: impl Iterator<Item = T>) -> ArenaHashMap {
     let mut map = ArenaHashMap::with_capacity(4);
-    for term in terms.split_whitespace() {
-        map.mutate_or_create(term.as_bytes(), |val| {
+    for term in terms {
+        map.mutate_or_create(term.as_ref(), |val| {
             if let Some(mut val) = val {
                 val += 1;
                 val
