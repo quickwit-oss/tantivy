2d48f0d0a9d3316f1e793eaf2f1eb8b62518718b
ChillFish8 <hburt2003@gmail.com>
2023-06-07 15:55:51 +0100

    Add missing type export

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -132,9 +132,10 @@ pub use self::bytes_options::BytesOptions;
 #[allow(deprecated)]
 pub use self::date_time_options::DatePrecision;
 pub use self::date_time_options::{DateOptions, DateTimePrecision, DATE_TIME_PRECISION_INDEXED};
+pub(crate) use self::document::doc_binary_wrappers;
 pub use self::document::{
-    doc_binary_wrappers, DocValue, Document, DocumentAccess, JsonValueVisitor, JsonVisitor,
-    SerializeJsonWrapper,
+    DocValue, Document, DocumentAccess, JsonValueVisitor, JsonVisitor,
+    SerializeJsonWrapper, ValueDeserialize
 };
 pub(crate) use self::facet::FACET_SEP_BYTE;
 pub use self::facet::{Facet, FacetParseError};
