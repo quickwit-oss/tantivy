f8d26ecb351f6fd8b9a3d0841815bef22d3f934e
Tony Xu <tonyx@amazon.com>
2023-06-08 23:02:17 -0700

    improvement: make WhitespaceTokenizer also handle unicode whitespaces

--- src/tokenizer/whitespace_tokenizer.rs
+++ src/tokenizer/whitespace_tokenizer.rs
@@ -30,7 +30,7 @@ impl<'a> WhitespaceTokenStream<'a> {
     // search for the end of the current token.
     fn search_token_end(&mut self) -> usize {
         (&mut self.chars)
-            .filter(|(_, c)| c.is_ascii_whitespace())
+            .filter(|(_, c)| c.is_whitespace())
             .map(|(offset, _)| offset)
             .next()
             .unwrap_or(self.text.len())
@@ -42,7 +42,7 @@ impl<'a> TokenStream for WhitespaceTokenStream<'a> {
         self.token.text.clear();
         self.token.position = self.token.position.wrapping_add(1);
         while let Some((offset_from, c)) = self.chars.next() {
-            if !c.is_ascii_whitespace() {
+            if !c.is_whitespace() {
                 let offset_to = self.search_token_end();
                 self.token.offset_from = offset_from;
                 self.token.offset_to = offset_to;
@@ -75,6 +75,26 @@ mod tests {
         assert_token(&tokens[1], 1, "happy", 7, 12);
         assert_token(&tokens[2], 2, "tax", 13, 16);
         assert_token(&tokens[3], 3, "payer!", 17, 23);
+    }
+
+    #[test]
+    fn test_unicode_whitespaces() {
+        let tokens = token_stream_helper("わ |　か　　花");
+        assert_eq!(tokens.len(), 4);
+
+        assert_eq!('わ'.len_utf8(), 3);
+        assert_token(&tokens[0], 0, "わ", 0, 3);
+
+        assert_eq!('|'.len_utf8(), 1);
+        assert_token(&tokens[1], 1, "|", 4, 5);
+
+        assert_eq!('　'.len_utf8(), 3);
+
+        assert_eq!('か'.len_utf8(), 3);
+        assert_token(&tokens[2], 2, "か", 8, 11);
+
+        assert_eq!('花'.len_utf8(), 3);
+        assert_token(&tokens[3], 3, "花", 17, 20);
     }
 
     fn token_stream_helper(text: &str) -> Vec<Token> {
