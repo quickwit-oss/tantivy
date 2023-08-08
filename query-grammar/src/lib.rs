b92082b748051301eb1dcb67763f56050af83ff4
trinity-1686a <trinity@quickwit.io>
2023-08-08 15:41:29 +0200

    implement lenient parser (#2129)
    * move query parser to nom
    
    * add suupport for term grouping
    
    * initial work on infallible parser
    
    * fmt
    
    * add tests and fix minor parsing bugs
    
    * address review comments
    
    * add support for lenient queries in tantivy
    
    * make lenient parser report errors
    
    * allow mixing occur and bool in query

--- query-grammar/src/lib.rs
+++ query-grammar/src/lib.rs
@@ -1,19 +1,26 @@
 #![allow(clippy::derive_partial_eq_without_eq)]
 
+mod infallible;
 mod occur;
 mod query_grammar;
 mod user_input_ast;
-use combine::parser::Parser;
 
+pub use crate::infallible::LenientError;
 pub use crate::occur::Occur;
-use crate::query_grammar::parse_to_ast;
+use crate::query_grammar::{parse_to_ast, parse_to_ast_lenient};
 pub use crate::user_input_ast::{
     Delimiter, UserInputAst, UserInputBound, UserInputLeaf, UserInputLiteral,
 };
 
 pub struct Error;
 
+/// Parse a query
 pub fn parse_query(query: &str) -> Result<UserInputAst, Error> {
-    let (user_input_ast, _remaining) = parse_to_ast().parse(query).map_err(|_| Error)?;
+    let (_remaining, user_input_ast) = parse_to_ast(query).map_err(|_| Error)?;
     Ok(user_input_ast)
+}
+
+/// Parse a query, trying to recover from syntax errors, and giving hints toward fixing errors.
+pub fn parse_query_lenient(query: &str) -> (UserInputAst, Vec<LenientError>) {
+    parse_to_ast_lenient(query)
 }
