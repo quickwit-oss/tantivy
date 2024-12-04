13d3b2f416cc6fc26386c60db7b6c55906553ac8
Anshul Goel <anshulgoel27@gmail.com>
2024-12-04 16:34:44 +0530

    updated range search code

--- examples/integer_range_search.rs
+++ examples/integer_range_search.rs
@@ -4,37 +4,53 @@ use std::ops::Bound;
 //
 // Below is an example of creating an indexed integer field in your schema
 // You can use RangeQuery to get a Count of all occurrences in a given range.
-use tantivy::collector::Count;
-use tantivy::query::RangeQuery;
-use tantivy::schema::{Schema, INDEXED};
-use tantivy::{doc, Index, IndexWriter, Result, Term};
+use tantivy::collector::{Count, TopDocs};
+use tantivy::query::{BooleanQuery, InvertedIndexRangeQuery, RangeQuery};
+use tantivy::schema::{Schema, FAST, INDEXED, STORED};
+use tantivy::{doc, Document, Index, IndexWriter, Result, TantivyDocument, Term};
 
 fn main() -> Result<()> {
     // For the sake of simplicity, this schema will only have 1 field
     let mut schema_builder = Schema::builder();
 
     // `INDEXED` is a short-hand to indicate that our field should be "searchable".
-    let year_field = schema_builder.add_u64_field("year", INDEXED);
+    let start_range = schema_builder.add_text_field("start_range", STORED | FAST);
+    let end_range = schema_builder.add_text_field("end_range",  STORED | FAST);
     let schema = schema_builder.build();
-    let index = Index::create_in_ram(schema);
+    let index = Index::create_in_ram(schema.clone());
     let reader = index.reader()?;
     {
-        let mut index_writer: IndexWriter = index.writer_with_num_threads(1, 6_000_000)?;
-        for year in 1950u64..2019u64 {
-            index_writer.add_document(doc!(year_field => year))?;
+        let mut index_writer: IndexWriter = index.writer_with_num_threads(1, 20_000_000)?;
+        // Add sample range data to the index
+        let ranges = vec![("10A", "20A"), ("B30", "B40"), ("5C0", "5C6")];
+
+        for (start, end) in ranges {
+            let mut doc = TantivyDocument::default();
+            doc.add_text(start_range, start);
+            doc.add_text(end_range, end);
+            index_writer.add_document(doc)?;
         }
         index_writer.commit()?;
-        // The index will be a range of years
     }
     reader.reload()?;
     let searcher = reader.searcher();
     // The end is excluded i.e. here we are searching up to 1969
-    let docs_in_the_sixties = RangeQuery::new(
-        Bound::Included(Term::from_field_u64(year_field, 1960)),
-        Bound::Excluded(Term::from_field_u64(year_field, 1970)),
+    let one = RangeQuery::new(
+        Bound::Unbounded,
+        Bound::Included(Term::from_field_text(start_range, "5C1")),
     );
-    // Uses a Count collector to sum the total number of docs in the range
-    let num_60s_books = searcher.search(&docs_in_the_sixties, &Count)?;
-    assert_eq!(num_60s_books, 10);
+    let two = RangeQuery::new(
+        Bound::Included(Term::from_field_text(end_range, "5C1")),
+        Bound::Unbounded,
+        
+    );
+    let query = BooleanQuery::intersection(vec![Box::new(one), Box::new(two)]);
+    let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
+
+    for (_score, doc_address) in top_docs {
+        let retrieved_doc: TantivyDocument = searcher.doc(doc_address)?;
+        println!("{}", retrieved_doc.to_json(&schema));
+    }
+
     Ok(())
 }
