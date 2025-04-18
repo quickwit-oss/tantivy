67c49eb8f80e22e049891881174b3621e9be2105
Kryesh <git@kryesh.tech>
2025-04-18 21:24:34 +1000

    Update `LogMergePolicy` to skip to a target number of documents per segment when possible

--- src/indexer/log_merge_policy.rs
+++ src/indexer/log_merge_policy.rs
@@ -19,7 +19,7 @@ const DEFAULT_DEL_DOCS_RATIO_BEFORE_MERGE: f32 = 1.0f32;
 #[derive(Debug, Clone)]
 pub struct LogMergePolicy {
     min_num_segments: usize,
-    max_docs_before_merge: usize,
+    target_segment_size: usize,
     min_layer_size: u32,
     level_log_size: f64,
     del_docs_ratio_before_merge: f32,
@@ -38,8 +38,8 @@ impl LogMergePolicy {
     /// Set the maximum number docs in a segment for it to be considered for
     /// merging. A segment can still reach more than max_docs, by merging many
     /// smaller ones.
-    pub fn set_max_docs_before_merge(&mut self, max_docs_merge_size: usize) {
-        self.max_docs_before_merge = max_docs_merge_size;
+    pub fn set_target_segment_size(&mut self, max_docs_merge_size: usize) {
+        self.target_segment_size = max_docs_merge_size;
     }
 
     /// Set the minimum segment size under which all segment belong
@@ -92,38 +92,87 @@ fn deletes_ratio(segment: &SegmentMeta) -> f32 {
 
 impl MergePolicy for LogMergePolicy {
     fn compute_merge_candidates(&self, segments: &[SegmentMeta]) -> Vec<MergeCandidate> {
+        // Get segments that are small enough to be merged and sort them by size in descending
+        // order.
         let size_sorted_segments = segments
             .iter()
-            .filter(|seg| seg.num_docs() <= (self.max_docs_before_merge as u32))
+            .filter(|seg| (seg.num_docs() as usize) < self.target_segment_size)
             .sorted_by_key(|seg| std::cmp::Reverse(seg.max_doc()))
-            .collect::<Vec<&SegmentMeta>>();
+            .collect_vec();
 
+        // If there are no small enough segments, return an empty vector.
         if size_sorted_segments.is_empty() {
-            return vec![];
+            return Vec::new();
         }
 
+        let mut candidates = Vec::new();
+        let mut levels = Vec::new();
+        let mut unmerged_docs = 0usize;
         let mut current_max_log_size = f64::MAX;
-        let mut levels = vec![];
         for (_, merge_group) in &size_sorted_segments.into_iter().chunk_by(|segment| {
             let segment_log_size = f64::from(self.clip_min_size(segment.num_docs())).log2();
             if segment_log_size < (current_max_log_size - self.level_log_size) {
                 // update current_max_log_size to create a new group
                 current_max_log_size = segment_log_size;
             }
+            // accumulate the number of documents
+            unmerged_docs += segment.num_docs() as usize;
             // return current_max_log_size to be grouped to the current group
             current_max_log_size
         }) {
             levels.push(merge_group.collect::<Vec<&SegmentMeta>>());
         }
 
+        // If the total number of unmerged documents is large enough to reach the target size,
+        // then start collecting segments in ascending size until we reach the target size.
+        if unmerged_docs >= self.target_segment_size {
+            let mut batch_docs = 0usize;
+            let mut batch = Vec::new();
+            // Pop segments segments from levels, smallest first due to sort at start
+            while let Some(segments) = levels.pop() {
+                for s in segments {
+                    batch_docs += s.num_docs() as usize;
+                    batch.push(s);
+
+                    // If the current batch has enough documents to be merged, create a merge
+                    // candidate and push it to candidates
+                    if batch_docs >= self.target_segment_size {
+                        unmerged_docs -= batch_docs;
+                        batch_docs = 0;
+                        candidates.push(MergeCandidate(
+                            // drain to reuse the buffer
+                            batch.drain(..).map(|seg| seg.id()).collect(),
+                        ));
+                    }
+                }
+
+                // If there are no longer enough documents to create a skip merge, break the loop
+                // unmerged_docs is only updated when a batch is created so this won't trigger
+                // before we have enough docs collected
+                if unmerged_docs <= self.target_segment_size {
+                    break;
+                }
+            }
+            // If there are any remaining segments in the batch, push them as a level to be
+            // processed by the standard merge policy
+            if !batch.is_empty() {
+                levels.push(batch);
+            }
+        }
+
         levels
-            .iter()
+            .into_iter()
             .filter(|level| {
                 level.len() >= self.min_num_segments
                     || self.has_segment_above_deletes_threshold(level)
             })
-            .map(|segments| MergeCandidate(segments.iter().map(|&seg| seg.id()).collect()))
-            .collect()
+            .for_each(|level| {
+                candidates.push(MergeCandidate(
+                    level.into_iter().map(|seg| seg.id()).collect(),
+                ))
+            });
+
+        candidates
     }
 }
 
@@ -131,7 +180,7 @@ impl Default for LogMergePolicy {
     fn default() -> LogMergePolicy {
         LogMergePolicy {
             min_num_segments: DEFAULT_MIN_NUM_SEGMENTS_IN_MERGE,
-            max_docs_before_merge: DEFAULT_MAX_DOCS_BEFORE_MERGE,
+            target_segment_size: DEFAULT_MAX_DOCS_BEFORE_MERGE,
             min_layer_size: DEFAULT_MIN_LAYER_SIZE,
             level_log_size: DEFAULT_LEVEL_LOG_SIZE,
             del_docs_ratio_before_merge: DEFAULT_DEL_DOCS_RATIO_BEFORE_MERGE,
@@ -163,7 +212,7 @@ mod tests {
         {
             let mut log_merge_policy = LogMergePolicy::default();
             log_merge_policy.set_min_num_segments(1);
-            log_merge_policy.set_max_docs_before_merge(1);
+            log_merge_policy.set_target_segment_size(1);
             log_merge_policy.set_min_layer_size(0);
 
             let mut index_writer = index.writer_for_tests()?;
@@ -214,7 +263,7 @@ mod tests {
     fn test_merge_policy() -> LogMergePolicy {
         let mut log_merge_policy = LogMergePolicy::default();
         log_merge_policy.set_min_num_segments(3);
-        log_merge_policy.set_max_docs_before_merge(100_000);
+        log_merge_policy.set_target_segment_size(100_000);
         log_merge_policy.set_min_layer_size(2);
         log_merge_policy
     }
@@ -318,14 +367,67 @@ mod tests {
             create_random_segment_meta(1_500_000),
         ];
         let result_list = test_merge_policy().compute_merge_candidates(&test_input);
-        // Do not include large segments
-        assert_eq!(result_list.len(), 1);
-        assert_eq!(result_list[0].0.len(), 3);
+        // All segments at or above target size, so we expect nothing
+        assert!(result_list.is_empty());
+    }
 
-        // Making sure merge policy points to the correct index of the original input
+    #[test]
+    fn test_skip_merge_large_segments() {
+        // Test that we skip log merges if there are enough unmerged documents to reach the target
+        // size
+        let test_input = vec![
+            create_random_segment_meta(50_000),
+            create_random_segment_meta(50_000),
+            create_random_segment_meta(49_999),
+            create_random_segment_meta(49_999),
+            create_random_segment_meta(49_999),
+            create_random_segment_meta(49_999),
+        ];
+
+        let result_list = test_merge_policy().compute_merge_candidates(&test_input);
+
+        assert_eq!(result_list.len(), 2);
+        // First result should be the first 2 segments being merged into a single 100k segment
+        assert_eq!(result_list[0].0.len(), 2);
+        assert_eq!(result_list[0].0[0], test_input[0].id());
+        assert_eq!(result_list[0].0[1], test_input[1].id());
+
+        // Second results should be the next 3 segments, excluding the final segment as it will have
+        // already hit the target
+        assert_eq!(result_list[1].0.len(), 3);
+        assert_eq!(result_list[1].0[0], test_input[2].id());
+        assert_eq!(result_list[1].0[1], test_input[3].id());
+        assert_eq!(result_list[1].0[2], test_input[4].id());
+    }
+
+    #[test]
+    fn test_skip_merge_small_segments() {
+        // Test that we skip log merges if there are enough unmerged documents to reach the target
+        // size
+        let test_input = vec![
+            create_random_segment_meta(75_000),
+            create_random_segment_meta(75_000),
+            create_random_segment_meta(5_000),
+            create_random_segment_meta(5_000),
+            create_random_segment_meta(5_000),
+            create_random_segment_meta(5_000),
+            create_random_segment_meta(5_000),
+        ];
+
+        let result_list = test_merge_policy().compute_merge_candidates(&test_input);
+
+        // Should have a single merge with all of the small segments and only one of the large
+        // segments
+        assert_eq!(result_list.len(), 1);
+        assert_eq!(result_list[0].0.len(), 6);
         assert_eq!(result_list[0].0[0], test_input[2].id());
-        assert_eq!(result_list[0].0[1], test_input[4].id());
-        assert_eq!(result_list[0].0[2], test_input[5].id());
+        assert_eq!(result_list[0].0[1], test_input[3].id());
+        assert_eq!(result_list[0].0[2], test_input[4].id());
+        assert_eq!(result_list[0].0[3], test_input[5].id());
+        assert_eq!(result_list[0].0[4], test_input[6].id());
+        assert!(
+            result_list[0].0[5] == test_input[0].id() || result_list[0].0[5] == test_input[1].id()
+        );
     }
 
     #[test]
