edc52c0bf1564f2063f53aeeac07c27b3dc5cec6
Ming <ming.ying.nyc@gmail.com>
2025-06-09 09:50:26 -0400

    chore: Make some delete-related functions public (#46)

--- src/indexer/mod.rs
+++ src/indexer/mod.rs
@@ -5,7 +5,7 @@
 //! [`Index::writer`](crate::Index::writer).
 
 /// Delete queue implementation for broadcasting delete operations to consumers.
-pub(crate) mod delete_queue;
+pub mod delete_queue;
 pub(crate) mod path_to_unordered_id;
 
 pub(crate) mod doc_id_mapping;
@@ -37,7 +37,7 @@ pub use self::index_writer::{advance_deletes, IndexWriter, IndexWriterOptions};
 pub use self::log_merge_policy::LogMergePolicy;
 pub use self::merge_operation::MergeOperation;
 pub use self::merge_policy::{MergeCandidate, MergePolicy, NoMergePolicy};
-pub use self::operation::{AddOperation, UserOperation};
+pub use self::operation::{AddOperation, DeleteOperation, UserOperation};
 pub use self::prepared_commit::PreparedCommit;
 pub use self::segment_entry::SegmentEntry;
 pub(crate) use self::segment_serializer::SegmentSerializer;
