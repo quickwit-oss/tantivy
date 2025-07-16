2a4b3f16837310134fa9f99e4f58ec09de4fc381
Kryesh <git@kryesh.tech>
2025-07-16 16:45:53 +1000

    Add tests for `RefReader`

--- common/src/refread.rs
+++ common/src/refread.rs
@@ -111,3 +111,126 @@ impl std::io::Read for RefReader {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::Read;
+
+    use ownedbytes::OwnedBytes;
+
+    use super::RefReader;
+
+    fn get_reader() -> RefReader {
+        RefReader::new(OwnedBytes::new(b"Hello, world!".to_vec()))
+    }
+
+    #[test]
+    fn test_read_n() {
+        let reader = get_reader();
+
+        let hello = reader.read_n(5).unwrap();
+        assert_eq!(hello, b"Hello");
+        assert_eq!(reader.pos(), 5);
+        assert_eq!(reader.remaining(), 8);
+
+        let comma_space = reader.read_n(2).unwrap();
+        assert_eq!(comma_space, b", ");
+        assert_eq!(reader.pos(), 7);
+        assert_eq!(reader.remaining(), 6);
+
+        let world = reader.read_n(6).unwrap();
+        assert_eq!(world, b"world!");
+        assert_eq!(reader.pos(), 13);
+        assert_eq!(reader.remaining(), 0);
+
+        assert!(reader.read_n(1).is_err());
+    }
+
+    #[test]
+    fn test_next_byte() {
+        let reader = get_reader();
+
+        assert_eq!(reader.next_byte().unwrap(), b'H');
+        assert_eq!(reader.next_byte().unwrap(), b'e');
+        assert_eq!(reader.next_byte().unwrap(), b'l');
+        assert_eq!(reader.next_byte().unwrap(), b'l');
+        assert_eq!(reader.next_byte().unwrap(), b'o');
+        assert_eq!(reader.next_byte().unwrap(), b',');
+        assert_eq!(reader.next_byte().unwrap(), b' ');
+        assert_eq!(reader.next_byte().unwrap(), b'w');
+        assert_eq!(reader.next_byte().unwrap(), b'o');
+        assert_eq!(reader.next_byte().unwrap(), b'r');
+        assert_eq!(reader.next_byte().unwrap(), b'l');
+        assert_eq!(reader.next_byte().unwrap(), b'd');
+        assert_eq!(reader.next_byte().unwrap(), b'!');
+
+        assert!(reader.next_byte().is_none());
+    }
+
+    #[test]
+    fn test_advance() {
+        let reader = get_reader();
+
+        reader.advance(7).unwrap();
+        assert_eq!(reader.read_n(6).unwrap(), b"world!");
+        assert!(reader.advance(1).is_err());
+    }
+
+    #[test]
+    fn test_reset() {
+        let reader = get_reader();
+
+        assert_eq!(reader.read_n(5).unwrap(), b"Hello");
+        assert_eq!(reader.pos(), 5);
+        assert_eq!(reader.remaining(), 8);
+        reader.reset();
+        assert_eq!(reader.read_n(5).unwrap(), b"Hello");
+        assert_eq!(reader.pos(), 5);
+        assert_eq!(reader.remaining(), 8);
+    }
+
+    #[test]
+    fn test_read_trait_exact() {
+        let mut reader = get_reader();
+
+        let mut buffer = [0; 5];
+        reader.read_exact(&mut buffer).unwrap();
+        assert_eq!(&buffer, b"Hello");
+        assert_eq!(reader.pos(), 5);
+        assert_eq!(reader.remaining(), 8);
+
+        let mut buffer = [0; 2];
+        reader.read_exact(&mut buffer).unwrap();
+        assert_eq!(&buffer, b", ");
+        assert_eq!(reader.pos(), 7);
+        assert_eq!(reader.remaining(), 6);
+
+        let mut buffer = [0; 6];
+        reader.read_exact(&mut buffer).unwrap();
+        assert_eq!(&buffer, b"world!");
+        assert_eq!(reader.pos(), 13);
+        assert_eq!(reader.remaining(), 0);
+
+        assert!(reader.read_exact(&mut buffer).is_err());
+    }
+
+    #[test]
+    fn test_read_trait() {
+        let mut reader = get_reader();
+
+        let mut small_buffer = [0; 5];
+        let bytes_read = reader.read(&mut small_buffer).unwrap();
+        assert_eq!(bytes_read, 5);
+        assert_eq!(&small_buffer, b"Hello");
+        assert_eq!(reader.pos(), 5);
+        assert_eq!(reader.remaining(), 8);
+
+        reader.reset();
+        let mut big_buffer = [0; 20];
+        let bytes_read = reader.read(&mut big_buffer).unwrap();
+        assert_eq!(bytes_read, 13);
+        assert_eq!(&big_buffer[..13], b"Hello, world!");
+        assert_eq!(reader.pos(), 13);
+        assert_eq!(reader.remaining(), 0);
+    }
+}
