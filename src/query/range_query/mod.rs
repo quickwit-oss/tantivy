18010b9aabcc9c7cfbbbe86998eedf05258b8a20
Pascal Seitz <pascal.seitz@datadoghq.com>
2025-09-11 19:19:34 +0200

    Add fast field fallback for term query if not indexed

--- src/query/range_query/mod.rs
+++ src/query/range_query/mod.rs
@@ -12,10 +12,14 @@ pub use self::range_query_fastfield::*;
 // TODO is this correct?
 pub(crate) fn is_type_valid_for_fastfield_range_query(typ: Type) -> bool {
     match typ {
-        Type::Str | Type::U64 | Type::I64 | Type::F64 | Type::Bool | Type::Date | Type::Json => {
-            true
-        }
-        Type::IpAddr => true,
+        Type::Str
+        | Type::U64
+        | Type::I64
+        | Type::F64
+        | Type::Bool
+        | Type::Date
+        | Type::Json
+        | Type::IpAddr => true,
         Type::Facet | Type::Bytes => false,
     }
 }
