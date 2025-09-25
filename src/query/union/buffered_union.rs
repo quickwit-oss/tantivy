b711d189a9ef8d4f601a800159f09ac7b711fe9d
Pascal Seitz <pascal.seitz@gmail.com>
2025-09-25 19:59:43 +0800

    fix debug assert

--- src/query/union/buffered_union.rs
+++ src/query/union/buffered_union.rs
@@ -17,9 +17,7 @@ const HORIZON: u32 = 64u32 * 64u32;
 //
 // Elements are dropped and not yielded.
 fn unordered_drain_filter<T, P>(v: &mut Vec<T>, mut predicate: P)
-where
-    P: FnMut(&mut T) -> bool,
-{
+where P: FnMut(&mut T) -> bool {
     let mut i = 0;
     while i < v.len() {
         if predicate(&mut v[i]) {
@@ -147,7 +145,8 @@ impl<TScorer: Scorer, TScoreCombiner: ScoreCombiner> BufferedUnionScorer<TScorer
     }
 
     fn is_in_horizon(&self, target: DocId) -> bool {
-        let gap = target - self.window_start_doc;
+        // wrapping_sub, because target may be < window_start_doc
+        let gap = target.wrapping_sub(self.window_start_doc);
         gap < HORIZON
     }
 }
