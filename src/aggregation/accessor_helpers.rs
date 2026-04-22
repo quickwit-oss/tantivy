5f01881ae2a25ed619dfa1007e6a341cd5a6f5e8
RJ Barman <rjhallsted@gmail.com>
2026-04-22 16:00:00 -0600

    fix: Missing sentinel is now considered in term agg requests with exclude + missing (#122)
    The missing sentinel value is computed as (this name is descriptive, not
    a reference ->) `max_term_ordinal + 1`. When there are zero unique
    terms, this causes the sentinel value to be 1 (because
    `max_term_ordinal` is 0 by default), which means the allowed terms
    bitset never contains it (because it will only have capacity 1 in this
    case).
    
    This fix adds logic to handle that by considering the number of unique
    values in the column when computing the missing sentinel value.
    
    (Ideally we'd have distinct representations for the max term ordinal of
    zero unique terms and one unique term, but that would be much broader
    refactor)
    
    (cherry picked from commit bed1dba0839f821681768425580e8c97f1a62c3e)

--- src/aggregation/accessor_helpers.rs
+++ src/aggregation/accessor_helpers.rs
@@ -7,6 +7,18 @@ use columnar::{Column, ColumnType};
 use crate::aggregation::{f64_to_fastfield_u64, Key};
 use crate::index::SegmentReader;
 
+/// For string columns, the missing value is always represented as column_max_value + 1, but in
+/// the case where we have no values for this column, that causes the missing value to be
+/// erroneously set to 1 (which would imply we had one additional value represented as 0).
+/// This checks for that case that case and returns 0 instead.
+fn missing_val_for_str_cols(column_max_value: u64, column_num_values: u32) -> u64 {
+    if column_num_values == 0 {
+        0
+    } else {
+        column_max_value + 1
+    }
+}
+
 /// Get the missing value as internal u64 representation
 ///
 /// For terms we use u64::MAX as sentinel value
@@ -17,15 +29,28 @@ use crate::index::SegmentReader;
 pub(crate) fn get_missing_val_as_u64_lenient(
     column_type: ColumnType,
     column_max_value: u64,
+    column_num_values: u32,
     missing: &Key,
     field_name: &str,
 ) -> crate::Result<Option<u64>> {
     let missing_val = match missing {
-        Key::Str(_) if column_type == ColumnType::Str => Some(column_max_value + 1),
+        Key::Str(_) if column_type == ColumnType::Str => Some(missing_val_for_str_cols(
+            column_max_value,
+            column_num_values,
+        )),
         // Allow fallback to number on text fields
-        Key::F64(_) if column_type == ColumnType::Str => Some(column_max_value + 1),
-        Key::U64(_) if column_type == ColumnType::Str => Some(column_max_value + 1),
-        Key::I64(_) if column_type == ColumnType::Str => Some(column_max_value + 1),
+        Key::F64(_) if column_type == ColumnType::Str => Some(missing_val_for_str_cols(
+            column_max_value,
+            column_num_values,
+        )),
+        Key::U64(_) if column_type == ColumnType::Str => Some(missing_val_for_str_cols(
+            column_max_value,
+            column_num_values,
+        )),
+        Key::I64(_) if column_type == ColumnType::Str => Some(missing_val_for_str_cols(
+            column_max_value,
+            column_num_values,
+        )),
         Key::F64(val) if column_type.numerical_type().is_some() => {
             f64_to_fastfield_u64(*val, &column_type)
         }
